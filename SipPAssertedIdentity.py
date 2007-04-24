8070cebe7dc951912bd1479ec0ccbcc36e4906de
Maxim Sobolev <sobomax@sippysoft.com>
2007-04-24 08:42:28 +0000

    Add CVS Ids.

--- SipPAssertedIdentity.py
+++ SipPAssertedIdentity.py
@@ -21,6 +21,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+#
+# $Id: SipPAssertedIdentity.py,v 1.2 2007/04/24 08:42:28 sobomax Exp $
 
 from SipFrom import SipFrom
 from SipGenericHF import SipGenericHF
--- SipWWWAuthenticate.py
+++ SipWWWAuthenticate.py
@@ -21,6 +21,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+#
+# $Id: SipWWWAuthenticate.py,v 1.2 2007/04/24 08:42:28 sobomax Exp $
 
 from random import random
 from md5 import md5
