8ad27ec002ef48df2c63f2c7d229c2239a082ca4
Maxim Sobolev <sobomax@sippysoft.com>
2008-03-18 04:49:19 +0000

    Actually fix big endian platforms.
    Reported by:	Ovidiu Sas

--- makeann.c
+++ makeann.c
@@ -24,7 +24,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: makeann.c,v 1.8 2008/03/18 01:27:33 sobomax Exp $
+ * $Id: makeann.c,v 1.9 2008/03/18 04:49:19 sobomax Exp $
  *
  */
 
@@ -57,7 +57,7 @@
 
 #if BYTE_ORDER == BIG_ENDIAN
 #define LE16_2_HOST(x) \
- (((((uint16_t)(x)) >> 8) & 0xff) | ((((uint16_t)(x)) & 0xff) << 8)
+ ((((uint16_t)(x)) >> 8) & 0xff) | ((((uint16_t)(x)) & 0xff) << 8)
 #else
 #define LE16_2_HOST(x) (x)
 #endif
