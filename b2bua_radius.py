49ecc55b66b5b34ae4190d573187f3a5adddf6ff
Maxim Sobolev <sobomax@sippysoft.com>
2008-08-07 20:06:09 +0000

    Make sure to strip port number when reporting remote IP in the originate accounting.

--- b2bua_radius.py
+++ b2bua_radius.py
@@ -24,7 +24,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #
-# $Id: b2bua_radius.py,v 1.31 2008/07/30 22:44:14 sobomax Exp $
+# $Id: b2bua_radius.py,v 1.32 2008/08/07 20:06:09 sobomax Exp $
 
 from Timeout import Timeout
 from Signal import Signal
@@ -287,13 +287,6 @@ class CallController:
         self.huntstop_scodes = parameters.get('huntstop_scodes', ())
         if self.global_config.has_key('static_tr_out'):
             cld = re_replace(self.global_config['static_tr_out'], cld)
-        if not forward_on_fail and self.global_config['acct_enable']:
-            self.acctO = RadiusAccounting(self.global_config, 'originate', send_start = self.global_config['start_acct_enable'])
-            self.acctO.setParams(parameters.get('bill-to', self.username), cli, parameters.get('bill-cld', cld), \
-              self.cGUID, self.cId, host, credit_time)
-        else:
-            self.acctO = None
-        self.acctA.credit_time = credit_time
         if host == 'sip-ua':
             host = self.source[0]
             port = self.source[1]
@@ -304,6 +297,13 @@ class CallController:
             else:
                 port = SipConf.default_port
             host = host[0]
+        if not forward_on_fail and self.global_config['acct_enable']:
+            self.acctO = RadiusAccounting(self.global_config, 'originate', send_start = self.global_config['start_acct_enable'])
+            self.acctO.setParams(parameters.get('bill-to', self.username), cli, parameters.get('bill-cld', cld), \
+              self.cGUID, self.cId, host[0], credit_time)
+        else:
+            self.acctO = None
+        self.acctA.credit_time = credit_time
         conn_handlers = [self.oConn]
         disc_handlers = []
         if not forward_on_fail and self.global_config['acct_enable']:
