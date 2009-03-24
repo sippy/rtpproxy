4bea6472811a0763774e21fa9c5fb66f6db780eb
Maxim Sobolev <sobomax@sippysoft.com>
2009-03-24 04:30:11 +0000

    Merge from trunk: solaris 10 compat fixes.

--- rtpp_command.c
+++ rtpp_command.c
@@ -24,14 +24,19 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: rtpp_command.c,v 1.24 2009/01/12 10:56:59 sobomax Exp $
+ * $Id: rtpp_command.c,v 1.24.2.1 2009/03/24 04:30:11 sobomax Exp $
  *
  */
+
+#include "config.h"
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/uio.h>
 #include <netinet/in.h>
+#ifdef HAVE_ALLOCA_H
+#include <alloca.h>
+#endif
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
