30549c611df9c5c233bb9c0dbc7040c9e86dac7b
Maxim Sobolev <sobomax@sippysoft.com>
2009-04-08 21:56:30 +0000

    Handle in-dialog OPTIONS. Some UAs are offended if we reject them.

--- UaStateConnected.py
+++ UaStateConnected.py
@@ -1,28 +1,3 @@
-# Copyright (c) 2003-2005 Maxim Sobolev. All rights reserved.
-# Copyright (c) 2006-2007 Sippy Software, Inc. All rights reserved.
-#
-# This file is part of SIPPY, a free RFC3261 SIP stack and B2BUA.
-#
-# SIPPY is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# For a license to use the SIPPY software under conditions
-# other than those described here, or to purchase support for this
-# software, please contact Sippy Software, Inc. by e-mail at the
-# following addresses: sales@sippysoft.com.
-#
-# SIPPY is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
-#
-# $Id: UaStateConnected.py,v 1.4 2008/09/24 09:25:38 sobomax Exp $
 
 from Timeout import Timeout
 from UaStateGeneric import UaStateGeneric
@@ -97,6 +72,9 @@ class UaStateConnected(UaStateGeneric):
             self.ua.global_config['sip_tm'].sendResponse(req.genResponse(200, 'OK'))
             self.ua.equeue.append(CCEventInfo(req.getBody(), rtime = req.rtime))
             return None
+        if req.getMethod() == 'OPTIONS':
+            self.ua.global_config['sip_tm'].sendResponse(req.genResponse(200, 'OK'))
+            return None
         #print 'wrong request %s in the state Connected' % req.getMethod()
         return None
 
@@ -160,7 +138,7 @@ class UaStateConnected(UaStateGeneric):
         if self.ua.state != self:
             return
         #self.ua.lSDP.parse()
-        #self.ua.lSDP.content.getF('m').body.port += 4
+        #self.ua.lSDP.content.m_header.port += 4
         req = self.ua.genRequest('INVITE', self.ua.lSDP)
         self.ua.lCSeq += 1
         self.triedauth = False
