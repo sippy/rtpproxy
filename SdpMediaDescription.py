0e5c2bff70900024d17168d5eff7a77563872f69
Maxim Sobolev <sobomax@sippysoft.com>
2009-08-17 01:27:08 +0000

    Add missed copyright notices.

--- SdpMediaDescription.py
+++ SdpMediaDescription.py
@@ -1,3 +1,27 @@
+# Copyright (c) 2009 Sippy Software, Inc. All rights reserved.
+#
+# This file is part of SIPPY, a free RFC3261 SIP stack and B2BUA.
+#
+# SIPPY is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# For a license to use the SIPPY software under conditions
+# other than those described here, or to purchase support for this
+# software, please contact Sippy Software, Inc. by e-mail at the
+# following addresses: sales@sippysoft.com.
+#
+# SIPPY is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+#
+# $Id: SdpMediaDescription.py,v 1.3 2009/08/17 01:27:08 sobomax Exp $
 
 from SdpConnecton import SdpConnecton
 from SdpMedia import SdpMedia
