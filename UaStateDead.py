5af21edc344dd9c156556b9513329fe8370bcd3a
Maxim Sobolev <sobomax@sippysoft.com>
2009-11-27 20:20:58 +0000

    Crear expire_timer, no_progress_timer and credit_timer just in case.

--- UaStateDead.py
+++ UaStateDead.py
@@ -22,7 +22,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #
-# $Id: UaStateDead.py,v 1.8 2009/11/19 02:09:30 sobomax Exp $
+# $Id: UaStateDead.py,v 1.9 2009/11/27 20:20:58 sobomax Exp $
 
 from UaStateGeneric import UaStateGeneric
 
@@ -41,6 +41,9 @@ class UaStateDead(UaStateGeneric):
         ua.fail_cbs = ()
         ua.on_local_sdp_change = None
         ua.on_remote_sdp_change = None
+        ua.expire_timer = None
+        ua.no_progress_timer = None
+        ua.credit_timer = None
         # Keep this at the very end of processing
         for callback in ua.dead_cbs:
             callback(ua)
