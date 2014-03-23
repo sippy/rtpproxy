64b193ccf72f1a4d0cbc00649a3d32221d481469
Maksym Sobolyev <sobomax@sippysoft.com>
2014-03-23 11:12:56 -0700

    Add (c).

--- rtpp_proc_async.h
+++ rtpp_proc_async.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Sippy Software, Inc., http://www.sippysoft.com
+ * Copyright (c) 2014 Sippy Software, Inc., http://www.sippysoft.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
