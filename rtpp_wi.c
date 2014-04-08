3421e43b59e1c50533ae35159c41188f8b417629
Maksym Sobolyev <sobomax@sippysoft.com>
2014-04-08 11:20:10 -0700

    Add rtpp_wi - a new module to manage "work items" that can be passed between different threads. Provide an interface to go from rtp_packet into wi quickly and efficiently, without doing extra allocation.

---
+++ rtpp_wi.c
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2014 Sippy Software, Inc., http://www.sippysoft.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "rtp.h"
+#include "rtpp_wi.h"
+#include "rtpp_wi_private.h"
+
+struct rtpp_wi *
+rtpp_wi_malloc(int sock, const void *msg, size_t msg_len, int flags,
+  const struct sockaddr *sendto, socklen_t tolen)
+{
+    struct rtpp_wi *wi;
+
+    wi = malloc(sizeof(struct rtpp_wi) + msg_len + tolen);
+    if (wi == NULL) {
+        return (NULL);
+    }
+    wi->free_ptr = wi;
+    wi->wi_type = RTPP_WI_TYPE_OPKT;
+    wi->sock = sock;
+    wi->flags = flags;
+    wi->msg = (char *)wi + sizeof(struct rtpp_wi);
+    wi->sendto = (struct sockaddr *)((char *)wi->msg + msg_len);
+    wi->msg_len = msg_len;
+    wi->nsend = 1;
+    memcpy(wi->msg, msg, msg_len);
+    wi->tolen = tolen;
+    memcpy(wi->sendto, sendto, tolen);
+    return (wi);
+}
+
+struct rtpp_wi *
+rtpp_wi_malloc_pkt(int sock, struct rtp_packet *pkt,
+  const struct sockaddr *sendto, socklen_t tolen, int nsend)
+{
+    struct rtpp_wi *wi;
+
+    if (pkt->size + tolen + sizeof(struct rtpp_wi) > sizeof(pkt->data.buf)) {
+        wi = rtpp_wi_malloc(sock, pkt->data.buf, pkt->size, 0, sendto, tolen);
+        rtp_packet_free(pkt);
+    } else {
+        wi = (struct rtpp_wi *)(pkt->data.buf + pkt->size);
+        wi->free_ptr = (struct rtpp_wi *)pkt;
+        wi->wi_type = RTPP_WI_TYPE_OPKT;
+        wi->sock = sock;
+        wi->flags = 0;
+        wi->msg = pkt->data.buf;
+        wi->sendto = (struct sockaddr *)((char *)wi + sizeof(struct rtpp_wi));
+        wi->msg_len = pkt->size;
+        wi->tolen = tolen;
+        memcpy(wi->sendto, sendto, tolen);
+    }
+    wi->nsend = nsend;
+    return (wi);
+}
+
+struct rtpp_wi *
+rtpp_wi_malloc_sgnl(int signum)
+{
+    struct rtpp_wi *wi;
+
+    wi = malloc(sizeof(struct rtpp_wi));
+    if (wi == NULL) {
+        return (NULL);
+    }
+    memset(wi, '\0', sizeof(struct rtpp_wi));
+    wi->free_ptr = wi;
+    wi->wi_type = RTPP_WI_TYPE_SGNL;
+    wi->flags = signum;
+    return (wi);
+}
+
+void
+rtpp_wi_free(struct rtpp_wi *wi)
+{
+
+    free(wi->free_ptr);
+}
