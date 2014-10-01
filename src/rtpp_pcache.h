bbd01b75a24b6f6608ac008045b0fcfeeafa2640
Maksym Sobolyev <sobomax@sippysoft.com>
2014-10-01 16:40:22 -0700

    DEFINE_METHOD is now belongs to rtpp_types.h.

--- src/rtpp_pcache.h
+++ src/rtpp_pcache.h
@@ -1,7 +1,9 @@
-#define DEFINE_METHOD(class, func, rval, args...) typedef rval (*func##_t)(struct class *, ## args)
-
 struct rtpp_pcache_obj;
 struct rtpp_pcache_fd;
+
+#if !defined(DEFINE_METHOD)
+#error "rtpp_types.h" needs to be included
+#endif
 
 DEFINE_METHOD(rtpp_pcache_obj, rtpp_pcache_obj_dtor, void);
 DEFINE_METHOD(rtpp_pcache_obj, rtpp_pcache_obj_open, struct rtpp_pcache_fd *, const char *);
