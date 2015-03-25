b1a1ebaac75731b4899be960ef41973217f52289
Maksym Sobolyev <sobomax@sippysoft.com>
2015-03-25 14:35:11 -0700

    Print SSRC using 0x%.8X format, so that it consistent with Wireshark.

--- extractaudio/main.c
+++ extractaudio/main.c
@@ -109,7 +109,7 @@ load_session(const char *path, struct channels *channels, enum origin origin)
     pcount = loader->load(loader, channels, &stat, origin);
 
     update_rtpp_totals(&stat);
-    printf("pcount=%u, min_seq=%u, max_seq=%u, seq_offset=%u, ssrc=%u, duplicates=%u\n",
+    printf("pcount=%u, min_seq=%u, max_seq=%u, seq_offset=%u, ssrc=0x%.8X, duplicates=%u\n",
       (unsigned int)stat.last.pcount, (unsigned int)stat.last.min_seq, (unsigned int)stat.last.max_seq,
       (unsigned int)stat.last.seq_offset, (unsigned int)stat.last.ssrc, (unsigned int)stat.last.duplicates);
     printf("ssrc_changes=%u, psent=%u, precvd=%u\n", stat.ssrc_changes, stat.psent, stat.precvd);
