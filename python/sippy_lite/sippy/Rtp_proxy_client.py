7bbe8b5f75fc6d05c9aca992b92964afe583b31f
Maksym Sobolyev <sobomax@sippysoft.com>
2015-05-15 14:04:08 -0700

    Add sippy_lite, enough of sippy stack to provide low-level rtpproxy client-side command interface.

---
+++ python/sippy_lite/sippy/Rtp_proxy_client.py
@@ -0,0 +1,257 @@
+# Copyright (c) 2003-2005 Maxim Sobolev. All rights reserved.
+# Copyright (c) 2006-2014 Sippy Software, Inc. All rights reserved.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from Timeout import Timeout
+from Rtp_proxy_client_udp import Rtp_proxy_client_udp
+from Rtp_proxy_client_local import Rtp_proxy_client_local
+
+from random import random
+import socket
+
+def randomize(x, p):
+    return x * (1.0 + p * (1.0 - 2.0 * random()))
+
+class Rtp_proxy_client(Rtp_proxy_client_udp, Rtp_proxy_client_local):
+    worker = None
+    address = None
+    online = False
+    copy_supported = False
+    stat_supported = False
+    tnot_supported = False
+    sbind_supported = False
+    shut_down = False
+    proxy_address = None
+    caps_done = False
+    sessions_created = None
+    active_sessions = None
+    active_streams = None
+    preceived = None
+    ptransmitted = None
+    hrtb_ival = 10.0
+    hrtb_retr_ival = 60.0
+
+    def __init__(self, global_config, *address, **kwargs):
+        #print 'Rtp_proxy_client', address
+        if len(address) == 0 and kwargs.has_key('spath'):
+            a = kwargs['spath']
+            del kwargs['spath']
+            if a.startswith('udp:'):
+                a = a.split(':', 2)
+                if len(a) == 2:
+                    rtppa = (a[1], 22222)
+                else:
+                    rtppa = (a[1], int(a[2]))
+                self.proxy_address = rtppa[0]
+                kwargs['family'] = socket.AF_INET
+                rtpp_class = Rtp_proxy_client_udp
+            elif a.startswith('udp6:'):
+                proto, a = a.split(':', 1)
+                if not a.endswith(']'):
+                    a = a.rsplit(':', 1)
+                    if len(a) == 1:
+                        rtp_proxy_host, rtp_proxy_port = a[0], 22222
+                    else:
+                        rtp_proxy_host, rtp_proxy_port = (a[0], int(a[1]))
+                else:
+                    rtp_proxy_host, rtp_proxy_port = a, 22222
+                if not rtp_proxy_host.startswith('['):
+                    rtp_proxy_host = '[%s]' % rtp_proxy_host
+                rtppa = (rtp_proxy_host, rtp_proxy_port)
+                self.proxy_address = rtppa[0]
+                kwargs['family'] = socket.AF_INET6
+                rtpp_class = Rtp_proxy_client_udp
+            else:
+                if a.startswith('unix:'):
+                    rtppa = a[5:]
+                elif a.startswith('cunix:'):
+                    rtppa = a[6:]
+                else:
+                    rtppa = a
+                self.proxy_address = global_config['_sip_address']
+                rtpp_class = Rtp_proxy_client_local
+            rtpp_class.__init__(self, global_config, rtppa, **kwargs)
+        elif len(address) > 0 and type(address[0]) in (tuple, list):
+            Rtp_proxy_client_udp.__init__(self, global_config, *address, \
+              **kwargs)
+            self.proxy_address = address[0]
+        else:
+            Rtp_proxy_client_local.__init__(self, global_config, *address, \
+              **kwargs)
+            self.proxy_address = global_config['_sip_address']
+        self.version_check()
+
+    def send_command(self, *args, **kwargs):
+        if self.is_local:
+            Rtp_proxy_client_local.send_command(self, *args, **kwargs)
+        else:
+            Rtp_proxy_client_udp.send_command(self, *args, **kwargs)
+
+    def caps_query1(self, result):
+        #print '%s.caps_query1(%s)' % (id(self), result)
+        if self.shut_down:
+            return
+        if not self.online:
+            return
+        if result != '1':
+            if result != None:
+                self.copy_supported = False
+                self.stat_supported = False
+                self.tnot_supported = False
+                self.sbind_supported = False
+                self.caps_done = True
+            return
+        self.copy_supported = True
+        self.send_command('VF 20080403', self.caps_query2)
+
+    def caps_query2(self, result):
+        #print '%s.caps_query2(%s)' % (id(self), result)
+        if self.shut_down:
+            return
+        if not self.online:
+            return
+        if result != None:
+            if result == '1':
+                self.stat_supported = True
+                self.send_command('VF 20081224', self.caps_query3)
+                return
+            else:
+                self.stat_supported = False
+                self.tnot_supported = False
+                self.sbind_supported = False
+                self.caps_done = True
+
+    def caps_query3(self, result):
+        #print '%s.caps_query3(%s)' % (id(self), result)
+        if self.shut_down:
+            return
+        if not self.online:
+            return
+        if result != None:
+            if result == '1':
+                self.tnot_supported = True
+            else:
+                self.tnot_supported = False
+            self.send_command('VF 20090810', self.caps_query4)
+            return
+
+    def caps_query4(self, result):
+        #print '%s.caps_query4(%s)' % (id(self), result)
+        if self.shut_down:
+            return
+        if not self.online:
+            return
+        if result != None:
+            if result == '1':
+                self.sbind_supported = True
+            else:
+                self.sbind_supported = False
+            self.caps_done = True
+
+    def version_check(self):
+        if self.shut_down:
+            return
+        self.send_command('V', self.version_check_reply)
+
+    def version_check_reply(self, version):
+        if self.shut_down:
+            return
+        if version == '20040107':
+            self.go_online()
+        elif self.online:
+            self.go_offline()
+        else:
+            Timeout(self.version_check, randomize(self.hrtb_retr_ival, 0.1))
+
+    def heartbeat(self):
+        #print 'heartbeat', self, self.address
+        if self.shut_down:
+            return
+        self.send_command('Ib', self.heartbeat_reply)
+
+    def heartbeat_reply(self, stats):
+        #print 'heartbeat_reply', self.address, stats, self.online
+        if self.shut_down:
+            return
+        if not self.online:
+            return
+        if stats == None:
+            self.active_sessions = None
+            self.go_offline()
+        else:
+            sessions_created = active_sessions = active_streams = preceived = ptransmitted = 0
+            for line in stats.splitlines():
+                line_parts = line.split(':', 1)
+                if line_parts[0] == 'sessions created':
+                    sessions_created = int(line_parts[1])
+                elif line_parts[0] == 'active sessions':
+                    active_sessions = int(line_parts[1])
+                elif line_parts[0] == 'active streams':
+                    active_streams = int(line_parts[1])
+                elif line_parts[0] == 'packets received':
+                    preceived = int(line_parts[1])
+                elif line_parts[0] == 'packets transmitted':
+                    ptransmitted = int(line_parts[1])
+                self.update_active(active_sessions, sessions_created, active_streams, preceived, ptransmitted)
+        Timeout(self.heartbeat, randomize(self.hrtb_ival, 0.1))
+
+    def go_online(self):
+        if self.shut_down:
+            return
+        if not self.online:
+            self.caps_done = False
+            self.send_command('VF 20071218', self.caps_query1)
+            self.online = True
+            self.heartbeat()
+
+    def go_offline(self):
+        if self.shut_down:
+            return
+        #print 'go_offline', self.address, self.online
+        if self.online:
+            self.online = False
+            Timeout(self.version_check, randomize(self.hrtb_retr_ival, 0.1))
+
+    def update_active(self, active_sessions, sessions_created, active_streams, preceived, ptransmitted):
+        self.sessions_created = sessions_created
+        self.active_sessions = active_sessions
+        self.active_streams = active_streams
+        self.preceived = preceived
+        self.ptransmitted = ptransmitted
+
+    def shutdown(self):
+        if self.shut_down: # do not crash when shutdown() called twice
+            return
+        self.shut_down = True
+        if self.is_local:
+            return Rtp_proxy_client_local.shutdown(self)
+        else:
+            return Rtp_proxy_client_udp.shutdown(self)
+
+    def get_rtpc_delay(self):
+        if self.is_local:
+            return Rtp_proxy_client_local.get_rtpc_delay(self)
+        else:
+            return Rtp_proxy_client_udp.get_rtpc_delay(self)
---
+++ python/sippy_lite/sippy/Udp_server.py
@@ -0,0 +1,312 @@
+# Copyright (c) 2003-2005 Maxim Sobolev. All rights reserved.
+# Copyright (c) 2006-2014 Sippy Software, Inc. All rights reserved.
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from twisted.internet import reactor
+from errno import ECONNRESET, ENOTCONN, ESHUTDOWN, EWOULDBLOCK, ENOBUFS, EAGAIN, \
+  EINTR
+from datetime import datetime
+from time import sleep, time
+from threading import Thread, Condition
+from random import random
+import socket
+import sys, traceback
+
+from Timeout import Timeout
+from Time.MonoTime import MonoTime
+
+class AsyncSender(Thread):
+    userv = None
+
+    def __init__(self, userv):
+        Thread.__init__(self)
+        self.userv = userv
+        self.setDaemon(True)
+        self.start()
+
+    def run(self):
+        while True:
+            self.userv.wi_available.acquire()
+            while len(self.userv.wi) == 0:
+                self.userv.wi_available.wait()
+            wi = self.userv.wi.pop(0)
+            if wi == None:
+                # Shutdown request, relay it further
+                self.userv.wi.append(None)
+                self.userv.wi_available.notify()
+            self.userv.wi_available.release()
+            if wi == None:
+                break
+            data, address = wi
+            try:
+                ai = socket.getaddrinfo(address[0], None, self.userv.uopts.family)
+            except:
+                continue
+            if self.userv.uopts.family == socket.AF_INET:
+                address = (ai[0][4][0], address[1])
+            else:
+                address = (ai[0][4][0], address[1], ai[0][4][2], ai[0][4][3])
+            for i in range(0, 20):
+                try:
+                    if self.userv.skt.sendto(data, address) == len(data):
+                        break
+                except socket.error, why:
+                    if why[0] not in (EWOULDBLOCK, ENOBUFS, EAGAIN):
+                        break
+                sleep(0.01)
+        self.userv = None
+
+class AsyncReceiver(Thread):
+    userv = None
+
+    def __init__(self, userv):
+        Thread.__init__(self)
+        self.userv = userv
+        self.setDaemon(True)
+        self.start()
+
+    def run(self):
+        maxemptydata = 100
+        while True:
+            try:
+                data, address = self.userv.skt.recvfrom(8192)
+                if not data:
+                    # Ugly hack to detect socket being closed under us on Linux.
+                    # The problem is that even call on non-closed socket can
+                    # sometimes return empty data buffer, making AsyncReceiver
+                    # to exit prematurely.
+                    maxemptydata -= 1
+                    if maxemptydata == 0:
+                        break
+                    continue
+                else:
+                    maxemptydata = 100
+            except Exception, why:
+                if isinstance(why, socket.error) and why[0] in (ECONNRESET, ENOTCONN, ESHUTDOWN):
+                    break
+                if isinstance(why, socket.error) and why[0] in (EINTR,):
+                    continue
+                else:
+                    print datetime.now(), 'Udp_server: unhandled exception when receiving incoming data'
+                    print '-' * 70
+                    traceback.print_exc(file = sys.stdout)
+                    print '-' * 70
+                    sys.stdout.flush()
+                    sleep(1)
+                    continue
+            rtime = MonoTime()
+            if self.userv.uopts.family == socket.AF_INET6:
+                address = ('[%s]' % address[0], address[1])
+            reactor.callFromThread(self.userv.handle_read, data, address, rtime)
+        self.userv = None
+
+_DEFAULT_FLAGS = socket.SO_REUSEADDR
+if hasattr(socket, 'SO_REUSEPORT'):
+    _DEFAULT_FLAGS |= socket.SO_REUSEPORT
+_DEFAULT_NWORKERS = 30
+
+class Udp_server_opts(object):
+    laddress = None
+    data_callback = None
+    family = None
+    flags = _DEFAULT_FLAGS
+    nworkers = _DEFAULT_NWORKERS
+    ploss_rate = 0.0
+    pdelay_max = 0.0
+
+    def __init__(self, laddress, data_callback, family = None, o = None):
+        if o == None:
+            if family == None:
+                if laddress != None and laddress[0].startswith('['):
+                    family = socket.AF_INET6
+                    laddress = (laddress[0][1:-1], laddress[1])
+                else:
+                    family = socket.AF_INET
+            self.family = family
+            self.laddress = laddress
+            self.data_callback = data_callback
+        else:
+            self.laddress, self.data_callback, self.family, self.nworkers, self.flags, \
+              self.ploss_rate, self.pdelay_max = o.laddress, o.data_callback, o.family, \
+              o.nworkers, o.flags, o.ploss_rate, o.pdelay_max
+
+    def getCopy(self):
+        return self.__class__(None, None, o = self)
+
+class Udp_server(object):
+    skt = None
+    uopts = None
+    sendqueue = None
+    stats = None
+    wi_available = None
+    wi = None
+    asenders = None
+    areceivers = None
+
+    def __init__(self, global_config, uopts):
+        self.uopts = uopts.getCopy()
+        self.skt = socket.socket(self.uopts.family, socket.SOCK_DGRAM)
+        if self.uopts.laddress != None:
+            ai = socket.getaddrinfo(self.uopts.laddress[0], None, self.uopts.family)
+            if self.uopts.family == socket.AF_INET:
+                address = (ai[0][4][0], self.uopts.laddress[1])
+            else:
+                address = (ai[0][4][0], self.uopts.laddress[1], ai[0][4][2], ai[0][4][3])
+            if (self.uopts.flags & socket.SO_REUSEADDR) != 0:
+                self.skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            if hasattr(socket, 'SO_REUSEPORT') and \
+              (self.uopts.flags & socket.SO_REUSEPORT) != 0:
+                self.skt.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+            self.skt.bind(address)
+        self.sendqueue = []
+        self.stats = [0, 0, 0]
+        self.wi_available = Condition()
+        self.wi = []
+        self.asenders = []
+        self.areceivers = []
+        for i in range(0, self.uopts.nworkers):
+            self.asenders.append(AsyncSender(self))
+            self.areceivers.append(AsyncReceiver(self))
+
+    def send_to(self, data, address, delayed = False):
+        if not isinstance(address, tuple):
+            raise Exception('Invalid address, not a tuple: %s' % str(address))
+        if self.uopts.ploss_rate > 0.0:
+            if random() < self.uopts.ploss_rate:
+                return
+        if self.uopts.pdelay_max > 0.0 and not delayed:
+            pdelay = self.uopts.pdelay_max * random()
+            Timeout(self.send_to, pdelay, 1, data, address, True)
+            return
+        addr, port = address
+        if self.uopts.family == socket.AF_INET6:
+            if not addr.startswith('['):
+                raise Exception('Invalid IPv6 address: %s' % addr)
+            address = (addr[1:-1], port)
+        self.wi_available.acquire()
+        self.wi.append((data, address))
+        self.wi_available.notify()
+        self.wi_available.release()
+ 
+    def handle_read(self, data, address, rtime):
+        if len(data) > 0 and self.uopts.data_callback != None:
+            self.stats[2] += 1
+            try:
+                self.uopts.data_callback(data, address, self, rtime)
+            except:
+                print datetime.now(), 'Udp_server: unhandled exception when processing incoming data'
+                print '-' * 70
+                traceback.print_exc(file = sys.stdout)
+                print '-' * 70
+                sys.stdout.flush()
+
+    def shutdown(self):
+        try:
+            self.skt.shutdown(socket.SHUT_RDWR)
+        except:
+            pass
+        self.wi_available.acquire()
+        self.wi.append(None)
+        self.wi_available.notify()
+        self.wi_available.release()
+        self.uopts.data_callback = None
+        for worker in self.asenders + self.areceivers:
+            worker.join()
+        self.asenders = None
+        self.areceivers = None
+
+class self_test(object):
+    from sys import exit
+    npongs = 2
+    ping_data = 'ping!'
+    ping_data6 = 'ping6!'
+    pong_laddr = None
+    pong_laddr6 = None
+    pong_data = 'pong!'
+    pong_data6 = 'pong6!'
+    ping_laddr = None
+    ping_laddr6 = None
+    ping_raddr = None
+    ping_raddr6 = None
+    pong_raddr = None
+    pong_raddr6 = None
+
+    def ping_received(self, data, address, udp_server, rtime):
+        if udp_server.uopts.family == socket.AF_INET:
+            print 'ping_received'
+            if data != self.ping_data or address != self.pong_raddr:
+                print data, address, self.ping_data, self.pong_raddr
+                exit(1)
+            udp_server.send_to(self.pong_data, address)
+        else:
+            print 'ping_received6'
+            if data != self.ping_data6 or address != self.pong_raddr6:
+                exit(1)
+            udp_server.send_to(self.pong_data6, address)
+
+    def pong_received(self, data, address, udp_server, rtime):
+        if udp_server.uopts.family == socket.AF_INET:
+            print 'pong_received'
+            if data != self.pong_data or address != self.ping_raddr:
+                exit(1)
+        else:
+            print 'pong_received6'
+            if data != self.pong_data6 or address != self.ping_raddr6:
+                exit(1)
+        self.npongs -= 1
+        if self.npongs == 0:
+            reactor.stop()
+
+    def run(self):
+        local_host = '127.0.0.1'
+        local_host6 = '[::1]'
+        remote_host = local_host
+        remote_host6 = local_host6
+        self.ping_laddr = (local_host, 12345)
+        self.pong_laddr = (local_host, 54321)
+        self.ping_laddr6 = (local_host6, 12345)
+        self.pong_laddr6 = (local_host6, 54321)
+        self.ping_raddr = (remote_host, 12345)
+        self.pong_raddr = (remote_host, 54321)
+        self.ping_raddr6 = (remote_host6, 12345)
+        self.pong_raddr6 = (remote_host6, 54321)
+        uopts_ping = Udp_server_opts(self.ping_laddr, self.ping_received)
+        uopts_ping6 = Udp_server_opts(self.ping_laddr6, self.ping_received)
+        uopts_pong = Udp_server_opts(self.pong_laddr, self.pong_received)
+        uopts_pong6 = Udp_server_opts(self.pong_laddr6, self.pong_received)
+        udp_server_ping = Udp_server({}, uopts_ping)
+        udp_server_pong = Udp_server({}, uopts_pong)
+        udp_server_pong.send_to(self.ping_data, self.ping_laddr)
+        udp_server_ping6 = Udp_server({}, uopts_ping6)
+        udp_server_pong6 = Udp_server({}, uopts_pong6)
+        udp_server_pong6.send_to(self.ping_data6, self.ping_laddr6)
+        reactor.run()
+        udp_server_ping.shutdown()
+        udp_server_pong.shutdown()
+        udp_server_ping6.shutdown()
+        udp_server_pong6.shutdown()
+
+if __name__ == '__main__':
+    self_test().run()
