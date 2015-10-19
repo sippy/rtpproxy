21e47557da10e1f4bb14e32d15194bf95211882a
Maksym Sobolyev <sobomax@sippysoft.com>
2015-10-19 14:01:46 -0700

    Add an option to also output realtime along with monotime.

--- python/getmonotime.py
+++ python/getmonotime.py
@@ -4,18 +4,24 @@ if __name__ == '__main__':
     sippy_path = None
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 's:S:i:o:b')
+        opts, args = getopt.getopt(sys.argv[1:], 'rS:')
     except getopt.GetoptError:
         usage()
 
+    out_realtime = False
     for o, a in opts:
         if o == '-S':
             sippy_path = a.strip()
             continue
+        if o == '-r':
+            out_realtime = True
 
     if sippy_path != None:
         sys.path.insert(0, sippy_path)
 
     from sippy.Time.clock_dtime import clock_getdtime, CLOCK_MONOTONIC
-    print clock_getdtime(CLOCK_MONOTONIC)
-
+    if not out_realtime:
+        print(clock_getdtime(CLOCK_MONOTONIC))
+    else:
+        from sippy.Time.clock_dtime import CLOCK_REALTIME
+        print("%f %f" % (clock_getdtime(CLOCK_MONOTONIC), clock_getdtime(CLOCK_REALTIME)))
