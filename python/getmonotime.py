72750a74e7581ca0b69195c0ebfd0dcd89c06b08
Maksym Sobolyev <sobomax@sippysoft.com>
2015-11-20 15:52:03 -0800

    o Add missing continue;
    o allow name of the desired mono clock to be passed in as
    an argument, so that we can properly work on different arches.

--- python/getmonotime.py
+++ python/getmonotime.py
@@ -4,24 +4,29 @@ if __name__ == '__main__':
     sippy_path = None
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'rS:')
+        opts, args = getopt.getopt(sys.argv[1:], 'rS:C:')
     except getopt.GetoptError:
         usage()
 
     out_realtime = False
+    clock_name = 'CLOCK_MONOTONIC'
     for o, a in opts:
         if o == '-S':
             sippy_path = a.strip()
             continue
         if o == '-r':
             out_realtime = True
+            continue
+        if o == '-C':
+            clock_name = a.strip()
+            continue
 
     if sippy_path != None:
         sys.path.insert(0, sippy_path)
 
-    from sippy.Time.clock_dtime import clock_getdtime, CLOCK_MONOTONIC
+    exec('from sippy.Time.clock_dtime import clock_getdtime, %s' % clock_name)
     if not out_realtime:
-        print(clock_getdtime(CLOCK_MONOTONIC))
+        print(clock_getdtime(eval(clock_name)))
     else:
         from sippy.Time.clock_dtime import CLOCK_REALTIME
-        print("%f %f" % (clock_getdtime(CLOCK_MONOTONIC), clock_getdtime(CLOCK_REALTIME)))
+        print("%f %f" % (clock_getdtime(eval(clock_name)), clock_getdtime(CLOCK_REALTIME)))
