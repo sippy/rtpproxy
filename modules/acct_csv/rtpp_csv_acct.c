1ffe555aad5d24ae41a927ac380d6345f3c5d43a
Maksym Sobolyev <sobomax@sippysoft.com>
2016-01-20 23:08:10 -0800

    Test module_strdup() and module_free().

--- modules/acct_csv/rtpp_csv_acct.c
+++ modules/acct_csv/rtpp_csv_acct.c
@@ -1,12 +1,14 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "rtpp_types.h"
 #include "rtpp_module.h"
 
 struct rtpp_module_priv {
    int foo;
+   char *baz;
 };
 
 static struct rtpp_module_priv *rtpp_csv_acct_ctor(struct rtpp_cfg_stable *);
@@ -26,6 +28,7 @@ rtpp_csv_acct_ctor(struct rtpp_cfg_stable *cfsp)
 {
 
     bar.foo = 123456;
+    bar.baz = module_strdup("hello, world!");
     return (&bar);
 }
 
@@ -34,5 +37,7 @@ rtpp_csv_acct_dtor(struct rtpp_module_priv *pvt)
 {
 
     assert(pvt->foo == 123456);
+    assert(strcmp(pvt->baz, "hello, world!") == 0);
+    module_free(pvt->baz);
     return;
 }
