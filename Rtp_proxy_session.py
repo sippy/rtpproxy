096065871e6c01c19078e41399c111a975c7fcdd
Maksym Sobolyev <sobomax@sippysoft.com>
2016-02-03 18:56:47 -0800

    Refactor most of the rtpproxy session code to eliminate most of the duplicate pieces and fold most of the routines into symmetrical per-side object. This removes a lot of duplication and API mostly is staying the same.

--- Rtp_proxy_session.py
+++ Rtp_proxy_session.py
@@ -36,19 +36,165 @@ import sys
 
 from twisted.internet import reactor
 
+class _rtpps_side(object):
+    session_exists = False
+    codecs = None
+    raddress = None
+    origin = None
+
+    def __init__(self):
+        self.origin = SdpOrigin()
+
+    def update(self, rtpps, remote_ip, remote_port, result_callback, options = '', index = 0, \
+      atype = 'IP4', *callback_parameters):
+        command = 'U'
+        rtpps.max_index = max(rtpps.max_index, index)
+        rtpc = rtpps.rtp_proxy_client
+        if rtpc.sbind_supported and self.raddress != None and atype == 'IP4':
+            if rtpc.is_local:
+                options += 'L%s' % rtpps.global_config['_sip_tm'].l4r.getServer( \
+                  self.raddress).uopts.laddress[0]
+            else:
+                options += 'R%s' % self.raddress[0]
+        command += options
+        from_tag, to_tag = self.gettags(rtpps)
+        otherside = self.getother(rtpps)
+        if otherside.session_exists:
+            command += ' %s %s %d %s %s' % ('%s-%d' % (rtpps.call_id, index), remote_ip, remote_port, from_tag, to_tag)
+        else:
+            command += ' %s %s %d %s' % ('%s-%d' % (rtpps.call_id, index), remote_ip, remote_port, from_tag)
+        if rtpps.notify_socket != None and index == 0 and \
+          rtpc.tnot_supported:
+            command += ' %s %s' % (rtpps.notify_socket, rtpps.notify_tag)
+        rtpc.send_command(command, self.update_result, (rtpps, result_callback, rtpc.proxy_address, \
+          callback_parameters))
+
+    def gettags(self, rtpps):
+        if self not in (rtpps.caller, rtpps.callee):
+            raise Exception("Corrupt Rtp_proxy_session")
+        if self == rtpps.caller:
+            return (rtpps.from_tag, rtpps.to_tag)
+        else:
+            return (rtpps.to_tag, rtpps.from_tag)
+
+    def getother(self, rtpps):
+        if self not in (rtpps.caller, rtpps.callee):
+            raise Exception("Corrupt Rtp_proxy_session")
+        if self == rtpps.caller:
+            return rtpps.callee
+        else:
+            return rtpps.caller
+
+    def update_result(self, result, args):
+        #print '%s.update_result(%s)' % (id(self), result)
+        rtpps, result_callback, proxy_address, callback_parameters = args
+        self.session_exists = True
+        if result == None:
+            result_callback(None, rtpps, *callback_parameters)
+            return
+        t1 = result.split()
+        rtpproxy_port = int(t1[0])
+        if rtpproxy_port == 0:
+            result_callback(None, rtpps, *callback_parameters)
+        family = 'IP4'
+        if len(t1) > 1:
+            rtpproxy_address = t1[1]
+            if len(t1) > 2 and t1[2] == '6':
+                family = 'IP6'
+        else:
+            rtpproxy_address = proxy_address
+        result_callback((rtpproxy_address, rtpproxy_port, family), rtpps, *callback_parameters)
+
+    def __play(self, result, rtpps, prompt_name, times, result_callback, index):
+        from_tag, to_tag = self.gettags(rtpps)
+        command = 'P%d %s %s %s %s %s' % (times, '%s-%d' % (rtpps.call_id, index), prompt_name, self.codecs, from_tag, to_tag)
+        rtpps.rtp_proxy_client.send_command(command, rtpps.command_result, result_callback)
+
+    def _play(self, rtpps, prompt_name, times = 1, result_callback = None, index = 0):
+        if not self.session_exists:
+            return
+        otherside = self.getother(rtpps)
+        if not otherside.session_exists:
+            otherside.update(rtpps, '0.0.0.0', 0, self.__play, '', index, 'IP4', prompt_name, times, result_callback, index)
+            return
+        self.__play(None, rtpps, prompt_name, times, result_callback, index)
+
+    def _stop_play(self, rtpps, result_callback = None, index = 0):
+        if not self.session_exists:
+            return
+        from_tag, to_tag = self.gettags(rtpps)
+        command = 'S %s %s %s' % ('%s-%d' % (rtpps.call_id, index), from_tag, to_tag)
+        rtpps.rtp_proxy_client.send_command(command, rtpps.command_result, result_callback)
+
+    def _on_sdp_change(self, rtpps, sdp_body, result_callback):
+        sects = []
+        try:
+            sdp_body.parse()
+        except Exception, exception:
+            print datetime.now(), 'can\'t parse SDP body: %s:' % str(exception)
+            print '-' * 70
+            print_exc(file = sys.stdout)
+            print '-' * 70
+            print sdp_body.content
+            print '-' * 70
+            sys.stdout.flush()
+            return
+        for i in range(0, len(sdp_body.content.sections)):
+            sect = sdp_body.content.sections[i]
+            if sect.m_header.transport.lower() not in ('udp', 'udptl', 'rtp/avp'):
+                continue
+            sects.append(sect)
+        if len(sects) == 0:
+            sdp_body.needs_update = False
+            result_callback(sdp_body)
+            return
+        formats = sects[0].m_header.formats
+        if len(formats) > 1:
+            self.codecs = reduce(lambda x, y: str(x) + ',' + str(y), formats)
+        else:
+            self.codecs = str(formats[0])
+        for sect in sects:
+            options = ''
+            if sect.c_header.atype == 'IP6':
+                options = '6'
+            self.update(rtpps, sect.c_header.addr, sect.m_header.port, self._sdp_change_finish, options, \
+              sects.index(sect), sect.c_header.atype, sdp_body, sect, sects, result_callback)
+        return
+
+    def _sdp_change_finish(self, address_port, rtpps, sdp_body, sect, sects, result_callback):
+        sect.needs_update = False
+        if address_port != None:
+            sect.c_header.atype = address_port[2]
+            sect.c_header.addr = address_port[0]
+            if sect.m_header.port != 0:
+                sect.m_header.port = address_port[1]
+        if len([x for x in sects if x.needs_update]) == 0:
+            sdp_body.content.o_header = self.origin.getCopy()
+            self.origin.version += 1
+            if rtpps.insert_nortpp:
+                sdp_body.content += 'a=nortpproxy:yes\r\n'
+            sdp_body.needs_update = False
+            result_callback(sdp_body)
+
+    def _copy(self, rtpps, remote_ip, remote_port, result_callback = None, index = 0):
+        if not self.session_exists:
+            self.update(self, '0.0.0.0', 0, self.__copy, '', index, 'IP4', remote_ip, remote_port, result_callback, index)
+            return
+        self.__copy(None, rtpps, remote_ip, remote_port, result_callback, index)
+
+    def __copy(self, result, rtpps, remote_ip, remote_port, result_callback = None, index = 0):
+        from_tag, to_tag = self.gettags(rtpps)
+        command = 'C %s udp:%s:%d %s %s' % ('%s-%d' % (rtpps.call_id, index), remote_ip, remote_port, from_tag, to_tag)
+        rtpps.rtp_proxy_client.send_command(command, rtpps.command_result, result_callback)
+
 class Rtp_proxy_session(object):
     rtp_proxy_client = None
     call_id = None
     from_tag = None
     to_tag = None
-    caller_session_exists = False
-    caller_codecs = None
-    caller_raddress = None
-    callee_session_exists = False
-    callee_codecs = None
-    callee_raddress = None
+    caller = None
+    callee = None
     max_index = -1
-    origin = None
     notify_socket = None
     notify_tag = None
     global_config = None
@@ -81,9 +227,10 @@ class Rtp_proxy_session(object):
             self.to_tag = to_tag
         else:
             self.to_tag = md5(str(random()) + str(time())).hexdigest()
-        self.origin = SdpOrigin()
         self.notify_socket = notify_socket
         self.notify_tag = notify_tag
+        self.caller = _rtpps_side()
+        self.callee = _rtpps_side()
 
     def version(self, result_callback):
         self.rtp_proxy_client.send_command('V', self.version_result, result_callback)
@@ -92,68 +239,30 @@ class Rtp_proxy_session(object):
         result_callback(result)
 
     def play_caller(self, prompt_name, times = 1, result_callback = None, index = 0):
-        if not self.caller_session_exists:
-            return
-        if not self.callee_session_exists:
-            self.update_callee('0.0.0.0', 0, self._play_caller, '', index, 'IP4', prompt_name, times, result_callback, index)
-            return
-        self._play_caller(None, prompt_name, times, result_callback, index)
-
-    def _play_caller(self, result, prompt_name, times, result_callback, index):
-        command = 'P%d %s %s %s %s %s' % (times, '%s-%d' % (self.call_id, index), prompt_name, self.caller_codecs, self.from_tag, self.to_tag)
-        self.rtp_proxy_client.send_command(command, self.command_result, result_callback)
+        return self.caller._play(self, prompt_name, times, result_callback, index)
 
     def play_callee(self, prompt_name, times = 1, result_callback = None, index = 0):
-        if not self.callee_session_exists:
-            return
-        if not self.caller_session_exists:
-            self.update_caller('0.0.0.0', 0, self._play_callee, '', index, 'IP4', prompt_name, times, result_callback, index)
-            return
-        self._play_callee(None, prompt_name, times, result_callback, index)
-
-    def _play_callee(self, result, prompt_name, times, result_callback, index):
-        command = 'P%d %s %s %s %s %s' % (times, '%s-%d' % (self.call_id, index), prompt_name, self.callee_codecs, self.to_tag, self.from_tag)
-        self.rtp_proxy_client.send_command(command, self.command_result, result_callback)
+        return self.callee._play(self, prompt_name, times, result_callback, index)
 
     def stop_play_caller(self, result_callback = None, index = 0):
-        if not self.caller_session_exists:
-            return
-        command = 'S %s %s %s' % ('%s-%d' % (self.call_id, index), self.from_tag, self.to_tag)
-        self.rtp_proxy_client.send_command(command, self.command_result, result_callback)
+        return self.caller._stop_play(self, result_callback, index)
 
     def stop_play_callee(self, result_callback = None, index = 0):
-        if not self.caller_session_exists:
-            return
-        command = 'S %s %s %s' % ('%s-%d' % (self.call_id, index), self.to_tag, self.from_tag)
-        self.rtp_proxy_client.send_command(command, self.command_result, result_callback)
+        return self.callee._stop_play(self, result_callback, index)
 
     def copy_caller(self, remote_ip, remote_port, result_callback = None, index = 0):
-        if not self.caller_session_exists:
-            self.update_caller('0.0.0.0', 0, self._copy_caller, '', index, 'IP4', remote_ip, remote_port, result_callback, index)
-            return
-        self._copy_caller(None, remote_ip, remote_port, result_callback, index)
-
-    def _copy_caller(self, result, remote_ip, remote_port, result_callback = None, index = 0):
-        command = 'C %s udp:%s:%d %s %s' % ('%s-%d' % (self.call_id, index), remote_ip, remote_port, self.from_tag, self.to_tag)
-        self.rtp_proxy_client.send_command(command, self.command_result, result_callback)
+        return self.caller._copy(self, remote_ip, remote_port, result_callback, index)
 
     def copy_callee(self, remote_ip, remote_port, result_callback = None, index = 0):
-        if not self.callee_session_exists:
-            self.update_callee('0.0.0.0', 0, self._copy_callee, '', index, 'IP4', remote_ip, remote_port, result_callback, index)
-            return
-        self._copy_callee(None, remote_ip, remote_port, result_callback, index)
-
-    def _copy_callee(self, result, remote_ip, remote_port, result_callback = None, index = 0):
-        command = 'C %s udp:%s:%d %s %s' % ('%s-%d' % (self.call_id, index), remote_ip, remote_port, self.to_tag, self.from_tag)
-        self.rtp_proxy_client.send_command(command, self.command_result, result_callback)
+        return self.callee._copy(self, remote_ip, remote_port, result_callback, index)
 
     def start_recording(self, rname = None, result_callback = None, index = 0):
-        if not self.caller_session_exists:
-            self.update_caller('0.0.0.0', 0, self._start_recording, '', index, 'IP4', rname, result_callback, index)
+        if not self.caller.session_exists:
+            self.caller.update(self, '0.0.0.0', 0, self._start_recording, '', index, 'IP4', rname, result_callback, index)
             return
-        self._start_recording(None, rname, result_callback, index)
+        self._start_recording(None, self, rname, result_callback, index)
 
-    def _start_recording(self, result, rname, result_callback, index):
+    def _start_recording(self, result, rtpps, rname, result_callback, index):
         if rname == None:
             command = 'R %s %s %s' % ('%s-%d' % (self.call_id, index), self.from_tag, self.to_tag)
             return self.rtp_proxy_client.send_command(command, self.command_result, result_callback)
@@ -171,69 +280,6 @@ class Rtp_proxy_session(object):
         if result_callback != None:
             result_callback(result)
 
-    def update_caller(self, remote_ip, remote_port, result_callback, options = '', index = 0, \
-      atype = 'IP4', *callback_parameters):
-        command = 'U'
-        self.max_index = max(self.max_index, index)
-        if self.rtp_proxy_client.sbind_supported and self.caller_raddress != None and atype == 'IP4':
-            if self.rtp_proxy_client.is_local:
-                options += 'L%s' % self.global_config['_sip_tm'].l4r.getServer( \
-                  self.caller_raddress).uopts.laddress[0]
-            else:
-                options += 'R%s' % self.caller_raddress[0]
-        command += options
-        if self.callee_session_exists:
-            command += ' %s %s %d %s %s' % ('%s-%d' % (self.call_id, index), remote_ip, remote_port, self.from_tag, self.to_tag)
-        else:
-            command += ' %s %s %d %s' % ('%s-%d' % (self.call_id, index), remote_ip, remote_port, self.from_tag)
-        if self.notify_socket != None and index == 0 and \
-          self.rtp_proxy_client.tnot_supported:
-            command += ' %s %s' % (self.notify_socket, self.notify_tag)
-        self.rtp_proxy_client.send_command(command, self.update_result, (result_callback, 'caller', callback_parameters))
-
-    def update_callee(self, remote_ip, remote_port, result_callback, options = '', index = 0, \
-      atype = 'IP4', *callback_parameters):
-        command = 'U'
-        self.max_index = max(self.max_index, index)
-        if self.rtp_proxy_client.sbind_supported and self.callee_raddress != None and atype == 'IP4':
-            if self.rtp_proxy_client.is_local:
-                options += 'L%s' % self.global_config['_sip_tm'].l4r.getServer( \
-                  self.callee_raddress).uopts.laddress[0]
-            else:
-                options += 'R%s' % self.callee_raddress[0]
-        command += options
-        if self.caller_session_exists:
-            command += ' %s %s %d %s %s' % ('%s-%d' % (self.call_id, index), remote_ip, remote_port, self.to_tag, self.from_tag)
-        else:
-            command += ' %s %s %d %s' % ('%s-%d' % (self.call_id, index), remote_ip, remote_port, self.to_tag)
-        if self.notify_socket != None and index == 0 \
-          and self.rtp_proxy_client.tnot_supported:
-            command += ' %s %s' % (self.notify_socket, self.notify_tag)
-        self.rtp_proxy_client.send_command(command, self.update_result, (result_callback, 'callee', callback_parameters))
-
-    def update_result(self, result, args):
-        #print '%s.update_result(%s)' % (id(self), result)
-        result_callback, face, callback_parameters = args
-        if face == 'caller':
-            self.caller_session_exists = True
-        else:
-            self.callee_session_exists = True
-        if result == None:
-            result_callback(None, *callback_parameters)
-            return
-        t1 = result.split()
-        rtpproxy_port = int(t1[0])
-        if rtpproxy_port == 0:
-            result_callback(None, *callback_parameters)
-        family = 'IP4'
-        if len(t1) > 1:
-            rtpproxy_address = t1[1]
-            if len(t1) > 2 and t1[2] == '6':
-                family = 'IP6'
-        else:
-            rtpproxy_address = self.rtp_proxy_client.proxy_address
-        result_callback((rtpproxy_address, rtpproxy_port, family), *callback_parameters)
-
     def delete(self):
         if self.rtp_proxy_client == None:
             return
@@ -244,65 +290,10 @@ class Rtp_proxy_session(object):
         self.rtp_proxy_client = None
 
     def on_caller_sdp_change(self, sdp_body, result_callback):
-        self.on_xxx_sdp_change(self.update_caller, sdp_body, result_callback)
+        self.caller._on_sdp_change(self, sdp_body, result_callback)
 
     def on_callee_sdp_change(self, sdp_body, result_callback):
-        self.on_xxx_sdp_change(self.update_callee, sdp_body, result_callback)
-
-    def on_xxx_sdp_change(self, update_xxx, sdp_body, result_callback):
-        sects = []
-        try:
-            sdp_body.parse()
-        except Exception, exception:
-            print datetime.now(), 'can\'t parse SDP body: %s:' % str(exception)
-            print '-' * 70
-            print_exc(file = sys.stdout)
-            print '-' * 70
-            print sdp_body.content
-            print '-' * 70
-            sys.stdout.flush()
-            return
-        for i in range(0, len(sdp_body.content.sections)):
-            sect = sdp_body.content.sections[i]
-            if sect.m_header.transport.lower() not in ('udp', 'udptl', 'rtp/avp'):
-                continue
-            sects.append(sect)
-        if len(sects) == 0:
-            sdp_body.needs_update = False
-            result_callback(sdp_body)
-            return
-        formats = sects[0].m_header.formats
-        if update_xxx == self.update_caller:
-            if len(formats) > 1:
-                self.caller_codecs = reduce(lambda x, y: str(x) + ',' + str(y), formats)
-            else:
-                self.caller_codecs = str(formats[0])
-        else:
-            if len(formats) > 1:
-                self.callee_codecs = reduce(lambda x, y: str(x) + ',' + str(y), formats)
-            else:
-                self.callee_codecs = str(formats[0])
-        for sect in sects:
-            options = ''
-            if sect.c_header.atype == 'IP6':
-                options = '6'
-            update_xxx(sect.c_header.addr, sect.m_header.port, self.xxx_sdp_change_finish, options, \
-              sects.index(sect), sect.c_header.atype, sdp_body, sect, sects, result_callback)
-        return
-
-    def xxx_sdp_change_finish(self, address_port, sdp_body, sect, sects, result_callback):
-        sect.needs_update = False
-        if address_port != None:
-            sect.c_header.atype = address_port[2]
-            sect.c_header.addr = address_port[0]
-            if sect.m_header.port != 0:
-                sect.m_header.port = address_port[1]
-        if len([x for x in sects if x.needs_update]) == 0:
-            sdp_body.content.o_header = self.origin
-            if self.insert_nortpp:
-                sdp_body.content += 'a=nortpproxy:yes\r\n'
-            sdp_body.needs_update = False
-            result_callback(sdp_body)
+        self.callee._on_sdp_change(self, sdp_body, result_callback)
 
     def __del__(self):
         if self.my_ident != get_ident():
