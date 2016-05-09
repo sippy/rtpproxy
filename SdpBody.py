4fb127ba2d2a3852bb028224b8055b70c66be7ba
Maksym Sobolyev <sobomax@sippysoft.com>
2016-05-09 15:18:43 -0700

    Validate top level header and per-session headers properly.

--- SdpBody.py
+++ SdpBody.py
@@ -52,7 +52,8 @@ class SdpBody(object):
     first_half = ('v', 'o', 's', 'i', 'u', 'e', 'p')
     second_half = ('b', 't', 'r', 'z', 'k')
     all_headers = ('v', 'o', 's', 'i', 'u', 'e', 'p', 'c', 'b', 't', 'r', 'z', 'k')
-    sect_hdrs_req = ('o', 's', 'c', 't')
+    top_hdrs_req = ('v', 'o', 's', 't')
+    sect_hdrs_req = ('c', 'm')
     sections = None
 
     def __init__(self, body = None, cself = None):
@@ -93,12 +94,13 @@ class SdpBody(object):
             if len(self.sections) == 0:
                 self.addHeader('c', c_header)
         # Do some sanity checking, RFC4566
-        if self.v_header == None:
-            raise Exception('Mandatory "v=" SDP header is missing')
+        for header_name in [x + '_header' for x in self.top_hdrs_req]:
+            if getattr(self, header_name) == None:
+                raise Exception('Mandatory "%=" session header is missing' % header_name[0])
         for section in self.sections:
             for header_name in [x + '_header' for x in self.sect_hdrs_req]:
                 if getattr(section, header_name) == None:
-                    raise Exception('Mandatory "%s=" SDP header is missing' % header_name[0])
+                    raise Exception('Mandatory "%s=" media header is missing' % header_name[0])
 
     def __str__(self):
         s = ''
