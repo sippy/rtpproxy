7488df93845121f96112a45d5b99fa7e13a7825e
Maksym Sobolyev <sobomax@sippysoft.com>
2016-05-09 15:09:12 -0700

    Make sure there is v= header at least on the top level and then either c= header on the global level and o=, s= and t= on the media level.
    SippySoft TT#:	SS-1698

--- SdpBody.py
+++ SdpBody.py
@@ -52,6 +52,7 @@ class SdpBody(object):
     first_half = ('v', 'o', 's', 'i', 'u', 'e', 'p')
     second_half = ('b', 't', 'r', 'z', 'k')
     all_headers = ('v', 'o', 's', 'i', 'u', 'e', 'p', 'c', 'b', 't', 'r', 'z', 'k')
+    sect_hdrs_req = ('o', 's', 'c', 't')
     sections = None
 
     def __init__(self, body = None, cself = None):
@@ -91,6 +92,13 @@ class SdpBody(object):
                     section.addHeader('c', c_header)
             if len(self.sections) == 0:
                 self.addHeader('c', c_header)
+        # Do some sanity checking, RFC4566
+        if self.v_header == None:
+            raise Exception('Mandatory "v=" SDP header is missing')
+        for section in self.sections:
+            for header_name in [x + '_header' for x in self.sect_hdrs_req]:
+                if getattr(section, header_name) == None:
+                    raise Exception('Mandatory "%s=" SDP header is missing' % header_name[0])
 
     def __str__(self):
         s = ''
