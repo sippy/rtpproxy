ee1cfdc10b98c4265decd78486f40479b48783b0
Maksym Sobolyev <sobomax@sippysoft.com>
2016-08-25 07:33:43 -0700

    o implement few remaining parts of the RFC4568
    o optimize structure for fitting all the streams in the session
    into one request. We still need to do the reply part to allow
    multiple results to be returned.

--- protos/out/srtp.py
+++ protos/out/srtp.py
@@ -1,43 +1,92 @@
 from crypto_pb2 import crypto_request, AES_CM_128_HMAC_SHA1_80
-from crypto_pb2 import km_inline
+from crypto_pb2 import km_inline, AES_CM_128_HMAC_SHA1_32, F8_128_HMAC_SHA1_80
+from crypto_pb2 import UNENCRYPTED_SRTCP, UNENCRYPTED_SRTP
 
-cr = crypto_request()
-co = cr.offer
-sinfo = co.stream_info
+cro = crypto_request()
+co = cro.offer
+sinfo = co.session
 sinfo.from_tag = 'from_tag1'
-sinfo.call_id = 'call_id1'
-cat = co.attributes.add()
+strm = co.streams.add()
+strm.info.idx = 0
+cat = strm.attributes.add()
 cat.inb.tag = 1
 cat.inb.suite = AES_CM_128_HMAC_SHA1_80
 cat.inb.key_method = km_inline
-cat.inb.key_info = 'UHXu88YuDj8guQxhCqEeue0CSr+JcQ/Uii4NdaS5'
-cat = co.attributes.add()
+ckey = cat.inb.keys.add()
+ckey.key_salt = 'UHXu88YuDj8guQxhCqEeue0CSr+JcQ/Uii4NdaS5'
+cat = strm.attributes.add()
 cat.inb.tag = 4
 cat.inb.suite = AES_CM_128_HMAC_SHA1_80
 cat.inb.key_method = km_inline
-cat.inb.key_info = 'NhqBK/JjtVJgmOgPyDKpvMTEjvFCQ4eiyVQelF6x'
+cat_prm = cat.inb.session_parameters.add()
+cat_prm.flag = UNENCRYPTED_SRTCP
+cat_prm = cat.inb.session_parameters.add()
+cat_prm.flag = UNENCRYPTED_SRTP
+cat_prm = cat.inb.session_parameters.add()
+cat_prm.kdr.value = 2
+ckey = cat.inb.keys.add()
+ckey.key_salt = 'NhqBK/JjtVJgmOgPyDKpvMTEjvFCQ4eiyVQelF6x'
+strm = co.streams.add()
+strm.info.idx = 1
+cat = strm.attributes.add()
+cat.inb.tag = 1
+cat.inb.suite = AES_CM_128_HMAC_SHA1_80
+cat.inb.key_method = km_inline
+ckey = cat.inb.keys.add()
+ckey.key_salt = 'd0RmdmcmVCspeEc3QGZiNWpVLFJhQX1cfHAwJSoj'
+ckey.lifetime = '2^20'
+ckey.mki.value = 1
+ckey.mki.length = 32
+cat = strm.attributes.add()
+cat.inb.tag = 2
+cat.inb.suite = F8_128_HMAC_SHA1_80
+cat.inb.key_method = km_inline
+ckey = cat.inb.keys.add()
+ckey.key_salt = 'MTIzNDU2Nzg5QUJDREUwMTIzNDU2Nzg5QUJjZGVm'
+ckey.lifetime = '2^20'
+ckey.mki.value = 1
+ckey.mki.length = 4
+ckey = cat.inb.keys.add()
+ckey.key_salt = 'QUJjZGVmMTIzNDU2Nzg5QUJDREUwMTIzNDU2Nzg5'
+ckey.lifetime = '2^20'
+ckey.mki.value = 2
+ckey.mki.length = 4
 print(len(co.SerializeToString()))
 #print(co.SerializeToString())
 
-cr = crypto_request()
-ca = cr.answer
-sinfo = ca.stream_info
+cra = crypto_request()
+ca = cra.answer
+sinfo = ca.session
 sinfo.from_tag = 'from_tag1'
 sinfo.to_tag = 'to_tag1'
-sinfo.call_id = 'call_id1'
-ca.attribute.inb.tag = 4
-ca.attribute.inb.suite = AES_CM_128_HMAC_SHA1_80
-ca.attribute.inb.key_method = km_inline
-ca.attribute.inb.key_info = '9sOd+W72ilwfNgNt0FzlGYj6YZPMqN1sJJx43UuS'
+strm = ca.streams.add()
+strm.info.idx = 0
+strm.attribute.inb.tag = 4
+strm.attribute.inb.suite = AES_CM_128_HMAC_SHA1_80
+strm.attribute.inb.key_method = km_inline
+ckey = strm.attribute.inb.keys.add()
+ckey.key_salt = '9sOd+W72ilwfNgNt0FzlGYj6YZPMqN1sJJx43UuS'
+strm = ca.streams.add()
+strm.info.idx = 1
+strm.attribute.inb.tag = 1
+strm.attribute.inb.suite = AES_CM_128_HMAC_SHA1_32
+strm.attribute.inb.key_method = km_inline
+ckey = strm.attribute.inb.keys.add()
+ckey.key_salt = 'NzB4d1BINUAvLEw6UzF3WSJ+PSdFcGdUJShpX1Zj'
+ckey.lifetime = '2^20'
+ckey.mki.value = 1
+ckey.mki.length = 32
 print(len(ca.SerializeToString()))
 
 from rtpp_request_pb2 import rtpp_request, CLS_MOD_CRYPTO
 req = rtpp_request()
-req.dest.mclass = CLS_MOD_CRYPTO;
-req.cmd_pb = co.SerializeToString()
+req.call_id = 'call_id1'
+req.dest.mclass = CLS_MOD_CRYPTO
+req.cmd_pb = cro.SerializeToString()
 print(len(req.SerializeToString()))
 
 req = rtpp_request()
-req.dest.mclass = CLS_MOD_CRYPTO;
-req.cmd_pb = ca.SerializeToString()
+req.call_id = 'call_id1'
+req.dest.mclass = CLS_MOD_CRYPTO
+req.cmd_pb = cra.SerializeToString()
 print(len(req.SerializeToString()))
