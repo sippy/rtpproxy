3f909e5ce2464a91d0817ba8f05b3c7597346cba
Maksym Sobolyev <sobomax@sippysoft.com>
2016-08-24 18:00:59 -0700

    Implement rtpp_request.

--- protos/out/srtp.py
+++ protos/out/srtp.py
@@ -1,7 +1,8 @@
-from crypto_pb2 import crypto_offer, crypto_answer, AES_CM_128_HMAC_SHA1_80
+from crypto_pb2 import crypto_request, AES_CM_128_HMAC_SHA1_80
 from crypto_pb2 import km_inline
 
-co = crypto_offer()
+cr = crypto_request()
+co = cr.offer
 sinfo = co.stream_info
 sinfo.from_tag = 'from_tag1'
 sinfo.call_id = 'call_id1'
@@ -18,7 +19,8 @@ cat.inb.key_info = 'NhqBK/JjtVJgmOgPyDKpvMTEjvFCQ4eiyVQelF6x'
 print(len(co.SerializeToString()))
 #print(co.SerializeToString())
 
-ca = crypto_answer()
+cr = crypto_request()
+ca = cr.answer
 sinfo = ca.stream_info
 sinfo.from_tag = 'from_tag1'
 sinfo.to_tag = 'to_tag1'
@@ -28,3 +30,14 @@ ca.attribute.inb.suite = AES_CM_128_HMAC_SHA1_80
 ca.attribute.inb.key_method = km_inline
 ca.attribute.inb.key_info = '9sOd+W72ilwfNgNt0FzlGYj6YZPMqN1sJJx43UuS'
 print(len(ca.SerializeToString()))
+
+from rtpp_request_pb2 import rtpp_request, CLS_MOD_CRYPTO
+req = rtpp_request()
+req.dest.mclass = CLS_MOD_CRYPTO;
+req.cmd_pb = co.SerializeToString()
+print(len(req.SerializeToString()))
+
+req = rtpp_request()
+req.dest.mclass = CLS_MOD_CRYPTO;
+req.cmd_pb = ca.SerializeToString()
+print(len(req.SerializeToString()))
