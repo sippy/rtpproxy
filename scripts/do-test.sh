1299f6cc5799b3c2cb9d6dedbbcf4e1485a05b01
Maksym Sobolyev <sobomax@sippysoft.com>
2017-02-21 14:27:06 -0800

    Add -y after apt-get install.

--- scripts/do-test.sh
+++ scripts/do-test.sh
@@ -19,7 +19,7 @@ make
 make clean
 #sudo pip install -r requirements.txt
 #sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install libgsm1-dev \
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libgsm1-dev \
  libsndfile1-dev tcpdump curl wireshark-common libsrtp0-dev
 tcpdump --version || true
 mkdir deps
