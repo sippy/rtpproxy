e883c5e8ba169ff2d0c7ca957fcaafd2570dd2eb
Maksym Sobolyev <sobomax@sippysoft.com>
2018-04-14 23:22:51 -0700

    Add some barebone accounting structure for the RTCP-HEP module.

---
+++ src/rtpp_acct_rtcp.c
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2004-2006 Maxim Sobolev <sobomax@FreeBSD.org>
+ * Copyright (c) 2006-2016 Sippy Software, Inc., http://www.sippysoft.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "rtpp_ssrc.h"
+#include "rtpa_stats.h"
+#include "rtpp_types.h"
+#include "rtpp_mallocs.h"
+#include "rtpp_refcnt.h"
+#include "rtpp_acct_rtcp.h"
+#include "rtpp_acct_rtcp_fin.h"
+#include "rtpp_netaddr.h"
+
+struct rtpp_acct_rtcp_priv {
+    struct rtpa_stats_jitter _jt;
+    struct rtpp_acct_rtcp pub;
+};
+
+static void rtpp_acct_rtcp_dtor(struct rtpp_acct_rtcp_priv *);
+
+#define PUB2PVT(pubp) \
+  ((struct rtpp_acct_rtcp_priv *)((char *)(pubp) - offsetof(struct rtpp_acct_rtcp_priv, pub)))
+
+struct rtpp_acct_rtcp *
+rtpp_acct_rtcp_ctor(uint64_t seuid)
+{
+    struct rtpp_acct_rtcp_priv *pvt;
+    struct rtpp_refcnt *rcnt;
+
+    pvt = rtpp_rzmalloc(sizeof(struct rtpp_acct_rtcp_priv), &rcnt);
+    if (pvt == NULL) {
+        goto e0;
+    }
+    pvt->pub.seuid = seuid;
+    pvt->pub.rcnt = rcnt;
+    pvt->pub.jt = &pvt->_jt;
+    CALL_SMETHOD(pvt->pub.rcnt, attach, (rtpp_refcnt_dtor_t)&rtpp_acct_rtcp_dtor,
+      pvt);
+    return ((&pvt->pub));
+
+e0:
+    return (NULL);
+}
+
+static void
+rtpp_acct_rtcp_dtor(struct rtpp_acct_rtcp_priv *pvt)
+{
+
+    /*rtpp_acct_rtcp_fin(&(pvt->pub));*/
+    if (pvt->pub.call_id != NULL)
+        free(pvt->pub.call_id);
+    free(pvt);
+}
+
+const unsigned int
+rtpp_acct_rtcp_osize(void)
+{
+
+    return (sizeof(struct rtpp_acct_rtcp_priv));
+}
