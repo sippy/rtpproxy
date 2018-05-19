e390168681aeee5d1fa7721c3e087f51073542a9
Maksym Sobolyev <sobomax@sippysoft.com>
2018-05-19 23:24:57 +0000

    Move config into a temporary header until we have some form of configuration parsing system in place.

--- modules/acct_rtcp_hep/rtpp_acct_rtcp_hep.c
+++ modules/acct_rtcp_hep/rtpp_acct_rtcp_hep.c
@@ -47,18 +47,7 @@
 #include "core_hep.h"
 #include "hep_api.h"
 
-static struct hep_ctx ctx = {
-        .initfails = 0,
-        .hints = {{ 0 }},
-        .capt_host  = "10.0.0.1",
-        .capt_port  = "9060",
-        .capt_proto = "udp",
-        .capt_id = 101,
-        .hep_version = 3,
-        .usessl = 0,
-        .pl_compress = 0,
-        .sendPacketsCount = 0
-};
+#include "_acct_rtcp_hep_config.h"
 
 struct rtpp_module_priv {
    int dummy;
