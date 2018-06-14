7155059e49de75298487956611a7cdea63579a7c
Maksym Sobolyev <sobomax@sippysoft.com>
2018-06-14 22:35:37 +0000

    Add rtcp.h, file that defines basic structures for the RTCP protocol. Derived from the rtp.h in the rtptools (c2c925e8638a).

---
+++ modules/acct_rtcp_hep/rtcp.h
@@ -0,0 +1,151 @@
+/*
+ * (c) 1998-2018 by Columbia University; all rights reserved
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _RTCP_H_
+#define _RTCP_H_
+
+/*
+ * rtcp.h  --  RTCP header file (RFC 1890)
+ */
+
+/*
+ * Current protocol version.
+ */
+#define RTP_VERSION    2
+
+#define RTP_SEQ_MOD (1<<16)
+#define RTP_MAX_SDES 255      /* maximum text length for SDES */
+
+typedef enum {
+    RTCP_SR   = 200,
+    RTCP_RR   = 201,
+    RTCP_SDES = 202,
+    RTCP_BYE  = 203,
+    RTCP_APP  = 204
+} rtcp_type_t;
+
+typedef enum {
+    RTCP_SDES_END   = 0,
+    RTCP_SDES_CNAME = 1,
+    RTCP_SDES_NAME  = 2,
+    RTCP_SDES_EMAIL = 3,
+    RTCP_SDES_PHONE = 4,
+    RTCP_SDES_LOC   = 5,
+    RTCP_SDES_TOOL  = 6,
+    RTCP_SDES_NOTE  = 7,
+    RTCP_SDES_PRIV  = 8
+} rtcp_sdes_type_t;
+
+/*
+ * RTCP common header word
+ */
+typedef struct {
+#if RTP_BIG_ENDIAN
+    unsigned int version:2;   /* protocol version */
+    unsigned int p:1;         /* padding flag */
+    unsigned int count:5;     /* varies by packet type */
+#else
+    unsigned int count:5;     /* varies by packet type */
+    unsigned int p:1;         /* padding flag */
+    unsigned int version:2;   /* protocol version */
+#endif
+    unsigned int pt:8;        /* RTCP packet type */
+    unsigned int length:16;   /* pkt len in words, w/o this word */
+} rtcp_common_t;
+
+/*
+ * Big-endian mask for version, padding bit and packet type pair
+ * FIXME?
+ */
+#define RTCP_VALID_MASK (0xc000 | 0x2000 | 0xfe)
+#define RTCP_VALID_VALUE ((RTP_VERSION << 14) | RTCP_SR)
+
+/*
+ * Reception report block
+ */
+typedef struct {
+    uint32_t ssrc;             /* data source being reported */
+    unsigned int fraction:8;  /* fraction lost since last SR/RR */
+    int lost:24;              /* cumul. no. pkts lost (signed!) */
+    uint32_t last_seq;         /* extended last seq. no. received */
+    uint32_t jitter;           /* interarrival jitter */
+    uint32_t lsr;              /* last SR packet from this source */
+    uint32_t dlsr;             /* delay since last SR packet */
+} rtcp_rr_t;
+
+/*
+ * SDES item
+ */
+typedef struct {
+    uint8_t type;             /* type of item (rtcp_sdes_type_t) */
+    uint8_t length;           /* length of item (in octets) */
+    char data[1];             /* text, not null-terminated */
+} rtcp_sdes_item_t;
+
+/*
+ * One RTCP packet
+ */
+typedef struct {
+    rtcp_common_t common;     /* common header */
+    union {
+        /* sender report (SR) */
+        struct {
+            uint32_t ssrc;     /* sender generating this report */
+            uint32_t ntp_sec;  /* NTP timestamp */
+            uint32_t ntp_frac;
+            uint32_t rtp_ts;   /* RTP timestamp */
+            uint32_t psent;    /* packets sent */
+            uint32_t osent;    /* octets sent */
+            rtcp_rr_t rr[1];  /* variable-length list */
+        } sr;
+
+        /* reception report (RR) */
+        struct {
+            uint32_t ssrc;     /* receiver generating this report */
+            rtcp_rr_t rr[1];  /* variable-length list */
+        } rr;
+
+        /* source description (SDES) */
+        struct rtcp_sdes {
+            uint32_t src;      /* first SSRC/CSRC */
+            rtcp_sdes_item_t item[1]; /* list of SDES items */
+        } sdes;
+
+        /* BYE */
+        struct {
+            uint32_t src[1];   /* list of sources */
+            /* can't express trailing text for reason */
+        } bye;
+    } r;
+} rtcp_t;
+
+typedef struct rtcp_sdes rtcp_sdes_t;
+
+#endif /* _RTCP_H_ */
