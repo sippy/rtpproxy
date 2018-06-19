84bd7482034615c99480c14587816bd776063820
Maksym Sobolyev <sobomax@sippysoft.com>
2018-06-19 09:43:34 -0700

    Actually parse config file and handle errors.

--- libucl/libucl_test.c
+++ libucl/libucl_test.c
@@ -1,4 +1,7 @@
 #include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include "ucl.h"
 
@@ -13,16 +16,52 @@ main(int argc, char **argv)
     struct ucl_parser *parser;
     ucl_object_t *conf_root;
     ucl_object_iter_t it_conf;
+    int fd;
+    const char *cfile;
 
     RTPP_MEMDEB_INIT(rtpproxy);
+
+    ecode = 0;
+
+    cfile = "libucl_test.conf";
+    fd = open(cfile, O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "open(\"%s\") failed\n", cfile);
+        ecode = 1;
+        goto e0;
+    }
+
     parser = ucl_parser_new(UCL_PARSER_NO_FILEVARS);
+    if (parser == NULL) {
+        fprintf(stderr, "ucl_parser_new() failed\n");
+        ecode = 1;
+        goto e1;
+    }
+
+    ucl_parser_add_fd(parser, fd);
     conf_root = ucl_parser_get_object(parser);
+    if (conf_root == NULL) {
+        fprintf(stderr, "ucl_parser_get_object() failed\n");
+        ecode = 1;
+        goto e2;
+    }
+    if (ucl_parser_get_error(parser)) {
+        fprintf(stderr, "Parse Error occured: %s\n", ucl_parser_get_error(parser));
+        ecode = 1;
+        goto e3;
+    }
+
     it_conf = ucl_object_iterate_new(conf_root);
     ucl_object_iterate_free(it_conf);
-    ucl_parser_free(parser);
-    ucl_object_unref(conf_root);
 
-    ecode = rtpp_memdeb_dumpstats(_rtpproxy_memdeb, 0) == 0 ? 0 : 1;
+e3:
+    ucl_object_unref(conf_root);
+e2:
+    ucl_parser_free(parser);
+e1:
+    close(fd);
+e0:
+    ecode = rtpp_memdeb_dumpstats(_rtpproxy_memdeb, 0) == 0 ? ecode : 1;
 
     return (ecode);
 }
