8801070251ffe70e28e6937b74ccbd57a984c2bc
Maksym Sobolyev <sobomax@sippysoft.com>
2018-06-21 16:58:09 -0700

    Add some UCL-related definitions to be shared by the RTPP code.

---
+++ src/rtpp_ucl.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2018 Sippy Software, Inc., http://www.sippysoft.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+/*
+ * Config parser helper callback function pointer alias
+ */
+typedef bool (*conf_helper_func)(const ucl_object_t *, const ucl_object_t *,
+  void *);
+
+/*
+ * Config parser helper callback map. Maps UCL keys to callback functions that
+ * parse the key and store the value in the correct struct member
+ */
+typedef struct conf_helper_callback_map {
+    const char *conf_key;
+    conf_helper_func callback;
+} conf_helper_map;
+
+struct conf_entry;
+
+bool rtpp_ucl_set_unknown(const ucl_object_t *top,
+  const ucl_object_t *obj, struct conf_entry *target __unused);
