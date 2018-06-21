9545f5395ce2b9bcd848bb2150a53805cbc8b198
Maksym Sobolyev <sobomax@sippysoft.com>
2018-06-21 16:56:39 -0700

    Add simple configuration parameters parser as well as a callout table.

---
+++ modules/acct_rtcp_hep/rtpp_arh_conf.c
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2018 Sippy Software, Inc., http://www.sippysoft.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <stdio.h>
+#include <strings.h>
+
+#include "ucl.h"
+
+#include "rtpp_ucl.h"
+
+struct hep_ctx;
+
+static bool
+conf_set_capt_host(const ucl_object_t *top, const ucl_object_t *obj, struct hep_ctx *target)
+{
+
+    const char *val = NULL;
+
+    val = ucl_object_tostring_forced(obj);
+    //target->capt_host = strdup(val);
+
+    return (true);
+}
+
+static bool
+conf_set_capt_port(const ucl_object_t *top, const ucl_object_t *obj, struct hep_ctx *target)
+{
+
+    const char *val = NULL;
+    int rport;
+
+    if (ucl_object_type(obj) == UCL_INT) {
+        rport = ucl_object_toint(obj);
+    } else {
+        val = ucl_object_tostring_forced(obj);
+        fprintf(stderr, "error in config file; invalid value for port in section '%s': '%s'\n",
+            ucl_object_key(obj), val);
+	return (false);
+    }
+    if (rport <= 0 || rport > 0xffff) {
+        fprintf(stderr, "error in config file; invalid value for port in section '%s': %d\n",
+            ucl_object_key(obj), rport);
+        return (false);
+    }
+    //target->capt_port = rport;
+    return (true);
+}
+
+static bool
+conf_set_capt_ptype(const ucl_object_t *top, const ucl_object_t *obj, struct hep_ctx *target)
+{
+
+    const char *val = NULL;
+
+    val = ucl_object_tostring_forced(obj);
+    if (strcasecmp(val, "udp") == 0) {
+        //target->hints = {{ .ai_socktype = SOCK_DGRAM }};
+        return (true);
+    } else if (strcasecmp(val, "tcp") == 0) {
+        //target->hints = {{ .ai_socktype = SOCK_STREAM }};
+        return (true);
+    }
+
+    fprintf(stderr, "error in config file; invalid value for ptype in section '%s': '%s'\n",
+      ucl_object_key(obj), val);
+    return (false);
+}
+
+static bool
+conf_set_capt_id(const ucl_object_t *top, const ucl_object_t *obj, struct hep_ctx *target)
+{
+
+    const char *val = NULL;
+    int capt_id;
+
+    if (ucl_object_type(obj) == UCL_INT) {
+        capt_id = ucl_object_toint(obj);
+    } else {
+        val = ucl_object_tostring_forced(obj);
+        fprintf(stderr, "error in config file; invalid value for capt_id in section '%s': '%s'\n",
+            ucl_object_key(obj), val);
+        return (false);
+    }
+    if (capt_id < 0 || capt_id > 0xffffffff) {
+        fprintf(stderr, "error in config file; invalid value for capt_id in section '%s': %d\n",
+            ucl_object_key(obj), capt_id);
+        return (false);
+    }
+    //target->capt_id = capt_id;
+    return (true);
+}
+
+static conf_helper_map _rtpp_arh_conf_map[] =
+{
+    { "load", NULL }, /* The "load" is set when the hep_ctx is created */
+    { "capt_host", (conf_helper_func) conf_set_capt_host },
+    { "capt_port", (conf_helper_func) conf_set_capt_port },
+    { "capt_ptype", (conf_helper_func) conf_set_capt_ptype },
+    { "capt_id", (conf_helper_func) conf_set_capt_id },
+    { NULL, (conf_helper_func) rtpp_ucl_set_unknown }
+};
+
+conf_helper_map *rtpp_arh_conf_map = _rtpp_arh_conf_map;
