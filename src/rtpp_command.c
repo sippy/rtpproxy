sippy/rtpproxy

commit 608fd7cc386da13e0d8223ebf8e5ff9c8af6c53a

Add initial support for sub-commands. Those are intended to be "on-success" modifiers to existing commands, such as U or L to be provided after && separator and re-using existing session lookup result. For now this is just a stub, we are yet to plug any real commands in here.

src/rtpp_command.c
@@ -347,6 +347,8 @@ get_command(struct cfg *cf, struct rtpp_ctrl_sock *rcsp, int controlfd, int *rva
     return (cmd);
 }
 
+#define ISAMPAMP(v) ((v)[0] == '&' && (v)[1] == '&' && (v)[2] == '\0')
+
 int
 rtpp_command_split(struct rtpp_command *cmd, int len, int *rval,
   struct rtpp_cmd_rcache *rcache_obj)
@@ -354,6 +356,7 @@ rtpp_command_split(struct rtpp_command *cmd, int len, int *rval,
     char **ap;
     char *cp;
     struct rtpp_command_priv *pvt;
+    struct rtpp_command_args *cap;
 
     PUB2PVT(cmd, pvt);
     if (len > 0 && cmd->buf[len - 1] == '\n') {
@@ -366,18 +369,29 @@ rtpp_command_split(struct rtpp_command *cmd, int len, int *rval,
     cmd->csp->ncmds_rcvd.cnt++;
 
     cp = cmd->buf;
-    for (ap = cmd->args.v; (*ap = rtpp_strsep(&cp, "\r\n\t ")) != NULL;) {
+    cap = &cmd->args;
+    for (ap = cap->v; (*ap = rtpp_strsep(&cp, "\r\n\t ")) != NULL;) {
         if (**ap != '\0') {
-            if (pvt->umode != 0 && cmd->args.c == 0 && pvt->cookie == NULL) {
-                pvt->cookie = *ap;
-                continue;
+            if (cap == &cmd->args) {
+                if (pvt->umode != 0 && cap->c == 0 && pvt->cookie == NULL) {
+                    pvt->cookie = *ap;
+                    continue;
+                }
+                if (ISAMPAMP(*ap)) {
+                    *ap = NULL;
+                    cap = &cmd->subc_args;
+                    ap = cap->v;
+                    continue;
+                }
             }
-            cmd->args.c++;
-            if (++ap >= &cmd->args.v[RTPC_MAX_ARGC])
-                break;
+            cap->c++;
+            if (++ap >= &cap->v[RTPC_MAX_ARGC])
+                goto etoomany;
         }
     }
-    if (cmd->args.c < 1 || (pvt->umode != 0 && pvt->cookie == NULL)) {
+    if (cmd->args.c < 1 || (pvt->umode != 0 && pvt->cookie == NULL) ||
+      (cap == &cmd->subc_args && cap->c < 1)) {
+etoomany:
         RTPP_LOG(pvt->cfs->glog, RTPP_LOG_ERR, "command syntax error");
         reply_error(cmd, ECODE_PARSE_1);
         *rval = GET_CMD_INVAL;

src/rtpp_command_ecodes.h
@@ -33,6 +33,7 @@
 
 #define ECODE_PARSE_NARGS  1
 #define ECODE_PARSE_MODS   2
+#define ECODE_PARSE_SUBC   3
 
 #define ECODE_PARSE_1      5
 #define ECODE_PARSE_2      6

src/rtpp_command_parse.c
@@ -50,6 +50,7 @@ struct cmd_props {
     int min_argc;
     int has_cmods;
     int has_call_id;
+    int has_subc;
     int fpos;
     int tpos;
     char *cmods;
@@ -61,6 +62,7 @@ fill_cmd_props(struct rtpp_cfg_stable *cfsp, struct rtpp_command *cmd,
 {
 
     cpp->has_call_id = 1;
+    cpp->has_subc = 0;
     cpp->fpos = -1;
     cpp->tpos = -1;
     cpp->cmods = &(cmd->args.v[0][1]);
@@ -250,6 +252,12 @@ rtpp_command_pre_parse(struct rtpp_cfg_stable *cfsp, struct rtpp_command *cmd)
         RTPP_LOG(cfsp->glog, RTPP_LOG_ERR, "%s command syntax error"
           ": modifiers are not supported by the command", cmd->cca.rname);
         reply_error(cmd, ECODE_PARSE_MODS);
+        return (-1);
+    }
+    if (cprops.has_subc == 0 && cmd->subc_args.c > 0) {
+        RTPP_LOG(cfsp->glog, RTPP_LOG_ERR, "%s command syntax error"
+          ": subcommand is not supported", cmd->cca.rname);
+        reply_error(cmd, ECODE_PARSE_SUBC);
         return (-1);
     }
     cmd->cca.call_id = cprops.has_call_id ? cmd->args.v[1] : NULL;

src/rtpp_command_private.h
@@ -80,6 +80,7 @@ struct rtpp_command
     char buf[RTPP_CMD_BUFLEN];
     char buf_t[256];
     struct rtpp_command_args args;
+    struct rtpp_command_args subc_args;
     struct sockaddr_storage raddr;
     struct sockaddr *laddr;
     socklen_t rlen;

src/rtpp_command_ver.c
@@ -60,6 +60,7 @@ static struct proto_cap proto_caps[] = {
     { "20150330", "Support for allocating a new port (\"Un\"/\"Ln\" commands)" },
     { "20150420", "Support for SEQ tracking and new rtpa_ counters; Q command extended" },
     { "20150617", "Support for the wildcard %%CC_SELF%% as a disconnect notify target" },
+    { "20191015", "Support for the && sub-command specifier" },
     { NULL, NULL }
 };
 

commit cd9fb48c9beb076deaaa9eb39abc6de434c68adf

Fix potential crash introduced in 608fd7cc386d if we call reply_error() too early, before pvt->rcache_obj has been set.

src/rtpp_command.c
@@ -349,6 +349,28 @@ get_command(struct cfg *cf, struct rtpp_ctrl_sock *rcsp, int controlfd, int *rva
 
 #define ISAMPAMP(v) ((v)[0] == '&' && (v)[1] == '&' && (v)[2] == '\0')
 
+static int
+rtpp_command_guard_retrans(struct rtpp_command *cmd,
+  struct rtpp_cmd_rcache *rcache_obj)
+{
+    size_t len;
+    struct rtpp_command_priv *pvt;
+
+    PUB2PVT(cmd, pvt);
+    if (CALL_METHOD(rcache_obj, lookup, pvt->cookie, pvt->buf_r,
+      sizeof(pvt->buf_r)) == 1) {
+        len = strlen(pvt->buf_r);
+        rtpp_anetio_sendto(pvt->cfs->rtpp_netio_cf, pvt->controlfd,
+          pvt->buf_r, len, 0, sstosa(&cmd->raddr), cmd->rlen);
+        cmd->csp->ncmds_rcvd.cnt--;
+        cmd->csp->ncmds_rcvd_ndups.cnt++;
+        return (1);
+    }
+    CALL_SMETHOD(rcache_obj->rcnt, incref);
+    pvt->rcache_obj = rcache_obj;
+    return (0);
+}
+
 int
 rtpp_command_split(struct rtpp_command *cmd, int len, int *rval,
   struct rtpp_cmd_rcache *rcache_obj)
@@ -373,8 +395,13 @@ rtpp_command_split(struct rtpp_command *cmd, int len, int *rval,
     for (ap = cap->v; (*ap = rtpp_strsep(&cp, "\r\n\t ")) != NULL;) {
         if (**ap != '\0') {
             if (cap == &cmd->args) {
+                /* Stream communication mode doesn't use cookie */
                 if (pvt->umode != 0 && cap->c == 0 && pvt->cookie == NULL) {
                     pvt->cookie = *ap;
+                    if (rtpp_command_guard_retrans(cmd, rcache_obj)) {
+                        *rval = GET_CMD_OK;
+                        return (1);
+                    }
                     continue;
                 }
                 if (ISAMPAMP(*ap)) {
@@ -396,22 +423,6 @@ etoomany:
         reply_error(cmd, ECODE_PARSE_1);
         *rval = GET_CMD_INVAL;
         return (1);
-    }
-
-    /* Stream communication mode doesn't use cookie */
-    if (pvt->umode != 0) {
-        if (CALL_METHOD(rcache_obj, lookup, pvt->cookie, pvt->buf_r,
-          sizeof(pvt->buf_r)) == 1) {
-            len = strlen(pvt->buf_r);
-            rtpp_anetio_sendto(pvt->cfs->rtpp_netio_cf, pvt->controlfd,
-              pvt->buf_r, len, 0, sstosa(&cmd->raddr), cmd->rlen);
-            cmd->csp->ncmds_rcvd.cnt--;
-            cmd->csp->ncmds_rcvd_ndups.cnt++;
-            *rval = GET_CMD_OK;
-            return (1);
-        }
-        CALL_SMETHOD(rcache_obj->rcnt, incref);
-        pvt->rcache_obj = rcache_obj;
     }
 
     /* Step I: parse parameters that are common to all ops */