7ded02194f81cf0842eda91188d1397fa531307e
Maksym Sobolyev <sobomax@sippysoft.com>
2019-11-03 00:38:04 -0700

    From work items born from rtp_packet provide specific deallocation routine.

--- src/rtpp_wi_pkt.c
+++ src/rtpp_wi_pkt.c
@@ -50,6 +50,7 @@ struct rtpp_wi_sendto {
 };
 
 static void rtpp_wi_free(struct rtpp_wi *);
+static void rtpp_wi_pkt_free(struct rtpp_wi *);
 
 static const struct rtpp_wi_sendto rtpp_wi_sendto_i = {
    .wip = {
@@ -94,7 +95,7 @@ rtpp_wi_malloc_pkt(int sock, struct rtp_packet *pkt,
     struct rtpp_wi_pvt *wipp;
 
     wipp = PUB2PVT(pkt->wi);
-    wipp->pub.dtor = rtpp_wi_free;
+    wipp->pub.dtor = rtpp_wi_pkt_free;
     wipp->pub.wi_type = RTPP_WI_TYPE_OPKT;
     wipp->free_ptr = (struct rtpp_wi *)pkt;
     wipp->sock = sock;
@@ -120,7 +121,7 @@ rtpp_wi_malloc_pkt_na(int sock, struct rtp_packet *pkt,
     struct rtpp_wi_pvt *wipp;
 
     wipp = PUB2PVT(pkt->wi);
-    wipp->pub.dtor = rtpp_wi_free;
+    wipp->pub.dtor = rtpp_wi_pkt_free;
     wipp->pub.wi_type = RTPP_WI_TYPE_OPKT;
     wipp->free_ptr = (struct rtpp_wi *)pkt;
     wipp->sock = sock;
@@ -150,4 +151,21 @@ rtpp_wi_free(struct rtpp_wi *wi)
         CALL_SMETHOD(wipp->log->rcnt, decref);
     }
     free(wipp->free_ptr);
+}
+
+static void
+rtpp_wi_pkt_free(struct rtpp_wi *wi)
+{
+    struct rtpp_wi_pvt *wipp;
+    struct rtp_packet *pkt;
+
+    wipp = PUB2PVT(wi);
+    if (wipp->sock_rcnt != NULL) {
+        CALL_SMETHOD(wipp->sock_rcnt, decref);
+    }
+    if (wipp->log != NULL) {
+        CALL_SMETHOD(wipp->log->rcnt, decref);
+    }
+    pkt = (struct rtp_packet *)wipp->free_ptr;
+    CALL_SMETHOD(pkt->rcnt, decref);
 }
