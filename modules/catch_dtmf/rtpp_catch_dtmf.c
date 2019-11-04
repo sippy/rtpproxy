3edb1e8fc3f34e607866e3c5ba03a5424dba215a
Maksym Sobolyev <sobomax@sippysoft.com>
2019-11-03 18:35:59 -0800

    Add some more boilerplate code to setup / teardown worker thread.

--- modules/catch_dtmf/rtpp_catch_dtmf.c
+++ modules/catch_dtmf/rtpp_catch_dtmf.c
@@ -1,21 +1,51 @@
+#include <pthread.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdlib.h>
 
 #include "rtpp_mallocs.h"
 #include "rtpp_types.h"
 #include "rtpp_refcnt.h"
+#include "rtpp_wi.h"
+#include "rtpp_wi_sgnl.h"
+#include "rtpp_queue.h"
 
 #include "../modules/catch_dtmf/rtpp_catch_dtmf.h"
 
 struct rtpp_catch_dtmf_pvt {
     struct rtpp_catch_dtmf pub;
+    struct rtpp_wi *sigterm;
+    struct rtpp_queue *q;
+    pthread_t worker;
 };
 
 static void
 rtpp_catch_dtmf_dtor(struct rtpp_catch_dtmf_pvt *pvt)
 {
 
+    rtpp_queue_put_item(pvt->sigterm, pvt->q);
+    pthread_join(pvt->worker, NULL);
+    rtpp_queue_destroy(pvt->q);
+    CALL_METHOD(pvt->sigterm, dtor);
     free(pvt);
+}
+
+static void
+rtpp_catch_dtmf_worker(void *arg)
+{
+    struct rtpp_catch_dtmf_pvt *pvt;
+    struct rtpp_wi *wi;
+    int signum;
+
+    pvt = (struct rtpp_catch_dtmf_pvt *)arg;
+    for (;;) {
+        wi = rtpp_queue_get_item(pvt->q, 0);
+        signum = rtpp_wi_sgnl_get_signum(wi);
+        if (signum == SIGTERM) {
+            break;
+        }
+        CALL_METHOD(wi, dtor);
+    }
 }
 
 struct rtpp_catch_dtmf *
@@ -26,9 +56,26 @@ rtpp_catch_dtmf_ctor(struct po_manager *pomp)
 
     pvt = rtpp_rzmalloc(sizeof(*pvt), &rcnt);
     if (pvt == NULL)
-        return (NULL);
+        goto e0;
     pvt->pub.rcnt = rcnt;
+    pvt->sigterm = rtpp_wi_malloc_sgnl(SIGTERM, NULL, 0);
+    if (pvt->sigterm == NULL)
+        goto e1;
+    pvt->q = rtpp_queue_init(1, "rtpp_catch_dtmf(%p)", pvt);
+    if (pvt->q == NULL)
+        goto e2;
+    if (pthread_create(&pvt->worker, NULL, (void *(*)(void *))rtpp_catch_dtmf_worker, pvt) != 0)
+        goto e3;
     CALL_SMETHOD(pvt->pub.rcnt, attach, (rtpp_refcnt_dtor_t)&rtpp_catch_dtmf_dtor,
       pvt);
     return (&(pvt->pub));
+e3:
+    rtpp_queue_destroy(pvt->q);
+e2:
+    CALL_METHOD(pvt->sigterm, dtor);
+e1:
+    CALL_SMETHOD(pvt->pub.rcnt, decref);
+    free(pvt);
+e0:
+    return (NULL);
 }
