3bdaade9d5f3b0ceb8dc415888790eb8353c5d1f
Maksym Sobolyev <sobomax@sippysoft.com>
2020-05-01 19:21:55 -0700

    Re-gen.

--- autosrc/rtpp_modman_fin.c
+++ autosrc/rtpp_modman_fin.c
@@ -7,6 +7,14 @@
 #include "rtpp_debug.h"
 #include "rtpp_modman.h"
 #include "rtpp_modman_fin.h"
+static void rtpp_modman_do_acct_fin(void *pub) {
+    fprintf(stderr, "Method rtpp_modman@%p::do_acct (rtpp_modman_do_acct) is invoked after destruction\x0a", pub);
+    RTPP_AUTOTRAP();
+}
+static void rtpp_modman_get_next_id_fin(void *pub) {
+    fprintf(stderr, "Method rtpp_modman@%p::get_next_id (rtpp_modman_get_next_id) is invoked after destruction\x0a", pub);
+    RTPP_AUTOTRAP();
+}
 static void rtpp_modman_insert_fin(void *pub) {
     fprintf(stderr, "Method rtpp_modman@%p::insert (rtpp_modman_insert) is invoked after destruction\x0a", pub);
     RTPP_AUTOTRAP();
@@ -16,6 +24,12 @@ static void rtpp_modman_startall_fin(void *pub) {
     RTPP_AUTOTRAP();
 }
 void rtpp_modman_fin(struct rtpp_modman *pub) {
+    RTPP_DBG_ASSERT(pub->do_acct != (rtpp_modman_do_acct_t)NULL);
+    RTPP_DBG_ASSERT(pub->do_acct != (rtpp_modman_do_acct_t)&rtpp_modman_do_acct_fin);
+    pub->do_acct = (rtpp_modman_do_acct_t)&rtpp_modman_do_acct_fin;
+    RTPP_DBG_ASSERT(pub->get_next_id != (rtpp_modman_get_next_id_t)NULL);
+    RTPP_DBG_ASSERT(pub->get_next_id != (rtpp_modman_get_next_id_t)&rtpp_modman_get_next_id_fin);
+    pub->get_next_id = (rtpp_modman_get_next_id_t)&rtpp_modman_get_next_id_fin;
     RTPP_DBG_ASSERT(pub->insert != (rtpp_modman_insert_t)NULL);
     RTPP_DBG_ASSERT(pub->insert != (rtpp_modman_insert_t)&rtpp_modman_insert_fin);
     pub->insert = (rtpp_modman_insert_t)&rtpp_modman_insert_fin;
@@ -44,14 +58,18 @@ rtpp_modman_fintest()
     tp = rtpp_rzmalloc(sizeof(*tp), offsetof(typeof(*tp), pub.rcnt));
     assert(tp != NULL);
     assert(tp->pub.rcnt != NULL);
+    tp->pub.do_acct = (rtpp_modman_do_acct_t)((void *)0x1);
+    tp->pub.get_next_id = (rtpp_modman_get_next_id_t)((void *)0x1);
     tp->pub.insert = (rtpp_modman_insert_t)((void *)0x1);
     tp->pub.startall = (rtpp_modman_startall_t)((void *)0x1);
     CALL_SMETHOD(tp->pub.rcnt, attach, (rtpp_refcnt_dtor_t)&rtpp_modman_fin,
       &tp->pub);
     RTPP_OBJ_DECREF(&(tp->pub));
+    CALL_TFIN(&tp->pub, do_acct);
+    CALL_TFIN(&tp->pub, get_next_id);
     CALL_TFIN(&tp->pub, insert);
     CALL_TFIN(&tp->pub, startall);
-    assert((_naborts - naborts_s) == 2);
+    assert((_naborts - naborts_s) == 4);
 }
 const static void *_rtpp_modman_ftp = (void *)&rtpp_modman_fintest;
 DATA_SET(rtpp_fintests, _rtpp_modman_ftp);
