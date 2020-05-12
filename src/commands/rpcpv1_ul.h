09a1ca3de8ad29bb03b6a8e0b8e7da8ef510a28d
Maksym Sobolyev <sobomax@sippysoft.com>
2020-05-12 15:19:23 -0700

    GC "All rights reserved".

--- src/commands/rpcpv1_ul.h
+++ src/commands/rpcpv1_ul.h
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2004-2006 Maxim Sobolev <sobomax@FreeBSD.org>
  * Copyright (c) 2006-2014 Sippy Software, Inc., http://www.sippysoft.com
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
