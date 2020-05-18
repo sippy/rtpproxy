45f2aab44c686883b14f7b7c2edea6c3fa0b4ec9
Maksym Sobolyev <sobomax@sippysoft.com>
2020-05-17 20:18:31 -0700

    Move overly long before_install into the script of its own to make it more modular allowing to append commands before/after.
    Add 3 variants of the Clang 9+LTO tests.

---
+++ scripts/ft-before_install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -e
+
+sudo -H DEBIAN_FRONTEND=noninteractive apt-get update
+sudo apt-get -y install python3-pip python-dev
+sudo pip3 install -U setuptools
+sudo pip3 install -U virtualenvwrapper
+which python
+python --version
+pip3 install --user cpp-coveralls
