26e50ffb6a90035c31dd79a2b61c973d29ac86e6
Maksym Sobolyev <sobomax@sippysoft.com>
2020-05-28 14:19:27 -0700

    Add some pre-broken modules to test error handling.

---
+++ modules/badmod/rtpp_badmod4.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020 Sippy Software, Inc., http://www.sippysoft.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <stdlib.h>
+
+#include "config_pp.h"
+
+#include "rtpp_module.h"
+#include "rtpp_module_acct.h"
+
+#ifdef RTPP_CHECK_LEAKS
+#include "rtpp_memdeb_internal.h"
+
+RTPP_MEMDEB_APP_STATIC;
+#endif
+
+static const struct rtpp_acct_handlers badmod4_aapi = {
+    .on_rtcp_rcvd = AAPI_FUNC(NULL + 1, 0)
+};
+struct rtpp_minfo rtpp_module = {
+    .descr.name = "badmod4",
+    .descr.ver = MI_VER_INIT(),
+#ifdef RTPP_CHECK_LEAKS
+    .memdeb_p = &MEMDEB_SYM,
+#endif
+    .aapi = &badmod4_aapi
+};
