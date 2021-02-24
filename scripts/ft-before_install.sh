356aa527304d137f63ab035ea78385e46a0d062f
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-24 10:10:26 -0800

    Install python wheels.

--- scripts/ft-before_install.sh
+++ scripts/ft-before_install.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo -H DEBIAN_FRONTEND=noninteractive apt-get update
 sudo apt-get -y install python3-pip python-dev
-sudo pip3 install -U setuptools
+sudo pip3 install -U pip setuptools wheel
 #sudo pip3 install -U virtualenvwrapper
 which python
 python --version
