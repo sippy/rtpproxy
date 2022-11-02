376ddc52bd31c8e10c9ddc9e3454c503cd66ed75
Maksym Sobolyev <sobomax@sippysoft.com>
2022-11-02 12:32:15 -0700

    Add DTLS encrypt/decrypt module, with crypto parts based loosely off the basesip's and SRTP decryption done by the libsrtp2.
    Add machinery to setup/destroy SSL/DTLS context.
    
    Add code to setup DTLS connection.
    
    Implement network I/O to and from TLS code.
    
    Add support for active mode and simple FSM to track state of
    the connection.
    
    Implement basic control protocol allowing to supply mode
    of remote "A" - for active, "P" for passive. For the case
    when we are initiating session the "S" (setup) is
    provided, roughly equivalent of "actpass". The "A" and "P"
    take the SHA-256 cert fingerprint and SSRC when available
    and return either success or failure, "S" takes no arguments
    and sends back our SHA-256 digest in response.
    
    Two more commands are provided to detach/destroy DTLS context:
    "D" and "U". "D" is to be used if we have received DTLS session,
    "U" if we have initiated.
    
    Only build dtls_gw when BUILD_CRYPTO is set.

---
+++ modules/dtls_gw/rtpp_dtls_conn.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022 Sippy Software, Inc., http://www.sippysoft.com
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+struct rtpp_refcnt;
+struct rtpp_stream;
+struct rtpp_dtls_conn;
+struct rtp_packet;
+struct rtpp_anetio_cf;
+struct pkt_proc_ctx;
+
+enum rtpp_dtls_mode {
+    RTPP_DTLS_MODERR  = -1,
+    RTPP_DTLS_ACTPASS =  0,
+    RTPP_DTLS_ACTIVE  =  1,
+    RTPP_DTLS_PASSIVE =  2
+};
+
+struct rdc_peer_spec {
+    enum rtpp_dtls_mode peer_mode;
+    const rtpp_str_t *algorithm;
+    const rtpp_str_t *fingerprint;
+    const rtpp_str_t *ssrc;
+};
+
+DEFINE_METHOD(rtpp_dtls_conn, rtpp_dtls_conn_dtls_recv, void,
+  const struct rtp_packet *);
+DEFINE_METHOD(rtpp_dtls_conn, rtpp_dtls_conn_rtp_send, int,
+  struct pkt_proc_ctx *);
+DEFINE_METHOD(rtpp_dtls_conn, rtpp_dtls_conn_srtp_recv, int,
+  struct pkt_proc_ctx *);
+DEFINE_METHOD(rtpp_dtls_conn, rtpp_dtls_conn_setmode, enum rtpp_dtls_mode,
+  const struct rdc_peer_spec *);
+
+struct rtpp_dtls_conn {
+    struct rtpp_refcnt *rcnt;
+    METHOD_ENTRY(rtpp_dtls_conn_dtls_recv, dtls_recv);
+    METHOD_ENTRY(rtpp_dtls_conn_rtp_send, rtp_send);
+    METHOD_ENTRY(rtpp_dtls_conn_srtp_recv, srtp_recv);
+    METHOD_ENTRY(rtpp_dtls_conn_setmode, setmode);
+};
+
+#if defined(OPENSSL_VERSION_NUMBER)
+struct rtpp_dtls_conn *rtpp_dtls_conn_ctor(const struct rtpp_cfg *,
+  SSL_CTX *, struct rtpp_stream *);
+#endif
---
+++ modules/dtls_gw/rtpp_dtls_util.h
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2022 Sippy Software, Inc., http://www.sippysoft.com
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#define FP_DIGEST_ALG "SHA-256"
+#define FP_DIGEST_ALG_LEN (sizeof(FP_DIGEST_ALG) - 1)
+#define FP_DIGEST_LEN 32
+#define FP_FINGERPRINT_STR_LEN ((FP_DIGEST_LEN * 2) + (FP_DIGEST_LEN - 1))
+#define FP_DIGEST_STR_LEN ((sizeof(FP_DIGEST_ALG) - 1) + 1 + \
+  FP_FINGERPRINT_STR_LEN)
+#define FP_DIGEST_STRBUF_LEN (FP_DIGEST_STR_LEN + 1)
+
+int rtpp_dtls_fp_gen(const X509 *, char *, int);
