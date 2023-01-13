2131becc5c92cbc0bc01d4684f592d2e97a6702b
Maksym Sobolyev <sobomax@sippysoft.com>
2023-01-12 23:16:49 -0800

    Use apt-spy2 to fix connectivity issues to azure.archive.ubuntu.com.

---
+++ scripts/ft-apt-get-update.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "${GHA_OS}" == "ubuntu-18.04" ]
+then
+  sudo gem install apt-spy2 -v 0.7.2
+else
+  sudo gem install apt-spy2
+fi
+sudo apt-spy2 check
+sudo apt-spy2 fix --commit
+sudo -H DEBIAN_FRONTEND=noninteractive apt-get update
