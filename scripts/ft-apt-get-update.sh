cfd1dd074e555812938a8bb0f056611f7056ea7d
Maksym Sobolyev <sobomax@sippysoft.com>
2023-01-19 15:08:06 -0800

    apt-spy2 was supposed to help with making builds more reliable, instead it became new single-point-of-failure causing jobs to fails in bulk. :(
    https://github.com/lagged/apt-spy2/issues/78

--- scripts/ft-apt-get-update.sh
+++ scripts/ft-apt-get-update.sh
@@ -3,12 +3,4 @@
 set -e
 set -x
 
-if [ "${GHA_OS}" = "ubuntu-18.04" ]
-then
-  sudo gem install apt-spy2 -v 0.7.2
-else
-  sudo gem install apt-spy2
-fi
-sudo apt-spy2 check --country=US
-sudo apt-spy2 fix --country=US --commit
-sudo -H DEBIAN_FRONTEND=noninteractive apt-get update
+sudo -H DEBIAN_FRONTEND=noninteractive apt-get update --fix-missing
