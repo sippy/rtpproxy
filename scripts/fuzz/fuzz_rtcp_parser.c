1c12abd02d2c5258da15f241a48462030490c671
Maksym Sobolyev <sobomax@sippysoft.com>
2023-02-18 15:07:01 -0800

    Don't exit(), return().

--- scripts/fuzz/fuzz_rtcp_parser.c
+++ scripts/fuzz/fuzz_rtcp_parser.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -12,8 +13,17 @@ LLVMFuzzerTestOneInput(const char *rtcp_data, size_t rtcp_dlen)
 
     sbp = rtpp_sbuf_ctor(512);
     assert (sbp != NULL);
+#if 0
+    for (size_t i = 0; i < rtcp_dlen; i++) {
+        char bf[3];
+        sprintf(bf, "%.2x", rtcp_data[i]);
+        write(STDERR_FILENO, bf, 2);
+    }
+    write(STDERR_FILENO, "\n", 1);
+    fsync(STDERR_FILENO);
+#endif
     rtcp2json(sbp, rtcp_data, rtcp_dlen);
     rtpp_sbuf_dtor(sbp);
 
-    exit(0);
+    return (0);
 }
