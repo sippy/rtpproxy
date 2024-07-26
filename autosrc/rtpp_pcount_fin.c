9c9af46eee682e2333a7d62b30da6d78e1cb317e
Maksym Sobolyev <sobomax@sippysoft.com>
2024-07-26 16:36:30 -0700

    Re-gen.

--- autosrc/rtpp_pcount_fin.c
+++ autosrc/rtpp_pcount_fin.c
@@ -11,6 +11,10 @@ static void rtpp_pcount_get_stats_fin(void *pub) {
     fprintf(stderr, "Method rtpp_pcount@%p::get_stats (rtpp_pcount_get_stats) is invoked after destruction\x0a", pub);
     RTPP_AUTOTRAP();
 }
+static void rtpp_pcount_log_drops_fin(void *pub) {
+    fprintf(stderr, "Method rtpp_pcount@%p::log_drops (rtpp_pcount_log_drops) is invoked after destruction\x0a", pub);
+    RTPP_AUTOTRAP();
+}
 static void rtpp_pcount_reg_drop_fin(void *pub) {
     fprintf(stderr, "Method rtpp_pcount@%p::reg_drop (rtpp_pcount_reg_drop) is invoked after destruction\x0a", pub);
     RTPP_AUTOTRAP();
@@ -25,12 +29,14 @@ static void rtpp_pcount_reg_reld_fin(void *pub) {
 }
 static const struct rtpp_pcount_smethods rtpp_pcount_smethods_fin = {
     .get_stats = (rtpp_pcount_get_stats_t)&rtpp_pcount_get_stats_fin,
+    .log_drops = (rtpp_pcount_log_drops_t)&rtpp_pcount_log_drops_fin,
     .reg_drop = (rtpp_pcount_reg_drop_t)&rtpp_pcount_reg_drop_fin,
     .reg_ignr = (rtpp_pcount_reg_ignr_t)&rtpp_pcount_reg_ignr_fin,
     .reg_reld = (rtpp_pcount_reg_reld_t)&rtpp_pcount_reg_reld_fin,
 };
 void rtpp_pcount_fin(struct rtpp_pcount *pub) {
     RTPP_DBG_ASSERT(pub->smethods->get_stats != (rtpp_pcount_get_stats_t)NULL);
+    RTPP_DBG_ASSERT(pub->smethods->log_drops != (rtpp_pcount_log_drops_t)NULL);
     RTPP_DBG_ASSERT(pub->smethods->reg_drop != (rtpp_pcount_reg_drop_t)NULL);
     RTPP_DBG_ASSERT(pub->smethods->reg_ignr != (rtpp_pcount_reg_ignr_t)NULL);
     RTPP_DBG_ASSERT(pub->smethods->reg_reld != (rtpp_pcount_reg_reld_t)NULL);
@@ -61,6 +67,7 @@ rtpp_pcount_fintest()
     assert(tp->pub.rcnt != NULL);
     static const struct rtpp_pcount_smethods dummy = {
         .get_stats = (rtpp_pcount_get_stats_t)((void *)0x1),
+        .log_drops = (rtpp_pcount_log_drops_t)((void *)0x1),
         .reg_drop = (rtpp_pcount_reg_drop_t)((void *)0x1),
         .reg_ignr = (rtpp_pcount_reg_ignr_t)((void *)0x1),
         .reg_reld = (rtpp_pcount_reg_reld_t)((void *)0x1),
@@ -70,10 +77,11 @@ rtpp_pcount_fintest()
       &tp->pub);
     RTPP_OBJ_DECREF(&(tp->pub));
     CALL_TFIN(&tp->pub, get_stats);
+    CALL_TFIN(&tp->pub, log_drops);
     CALL_TFIN(&tp->pub, reg_drop);
     CALL_TFIN(&tp->pub, reg_ignr);
     CALL_TFIN(&tp->pub, reg_reld);
-    assert((_naborts - naborts_s) == 4);
+    assert((_naborts - naborts_s) == 5);
     free(tp);
 }
 const static void *_rtpp_pcount_ftp = (void *)&rtpp_pcount_fintest;
