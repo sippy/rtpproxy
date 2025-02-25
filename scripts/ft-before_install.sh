2f5251f2430ba61cb820c65f66ae16d4a17a0c5e
Maksym Sobolyev <sobomax@sippysoft.com>
2025-02-24 17:08:35 -0800

    Remove Ubuntu 20.04 (to be discontinued by the github) and add 24.04.

--- scripts/ft-before_install.sh
+++ scripts/ft-before_install.sh
@@ -6,8 +6,7 @@ set -e
 . $(dirname $0)/build/build.conf.sub
 
 ${SUDO} apt-get -y install python3-pip python3-dev
-${SUDO} pip3 install -U pip setuptools wheel
-#${SUDO} pip3 install -U virtualenvwrapper
+${SUDO} pip3 install --user -U pip setuptools
 which python
 python --version
 for pkg in parsimonious elperiodic cpp-coveralls
