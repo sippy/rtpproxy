e16a796b78f50240567c5373dba3c878dd339d4d
Maksym Sobolyev <sobomax@sippysoft.com>
2025-10-27 22:07:45 -0700

    Switch bionic to use https for packages, we are having issues updating over http.

--- scripts/ft-apt-get-update.sh
+++ scripts/ft-apt-get-update.sh
@@ -7,5 +7,13 @@ set -x
 . $(dirname $0)/build/build.conf.sub
 
 ${SUDO} apt-get update -y --fix-missing
+
+if test -f /etc/apt/sources.list && grep -q bionic /etc/apt/sources.list
+then
+  ${SUDO} apt-get install -y ca-certificates
+  ${SUDO} perl -pi -e 's|http://|https://|g' /etc/apt/sources.list
+  ${SUDO} apt-get update -y --fix-missing
+fi
+
 ${SUDO} apt-mark hold grub-efi-amd64-signed
 ${SUDO} apt-get autoclean
