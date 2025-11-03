1d68cee41fe28af86563b53fd4f1936536cef1c7
Maksym Sobolyev <sobomax@sippysoft.com>
2025-11-03 09:34:19 -0800

    Add libatomic1 when building on linux/arm/v5.

--- scripts/build/get-arch-buildargs.sh
+++ scripts/build/get-arch-buildargs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+set -x
 
 platformopts() {
   out=""
@@ -11,6 +12,20 @@ platformopts() {
       echo "QEMU_CPU=cortex-a53"
       ;;
     esac
+    ;;
+  debian:*)
+    case "${TARGETPLATFORM}" in
+    linux/arm/v5)
+      if [ -z "${LIBS}" ]
+      then
+        echo "LIBS=-latomic"
+      else
+        echo "LIBS=${LIBS} -latomic"
+      fi
+      echo "LIB_DEPS=${LIB_DEPS} libatomic1"
+      ;;
+    esac
+    ;;
   esac
 }
 
