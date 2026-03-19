cab527f3a26e36fd62905aa8615ee581c25d50fd
Maksym Sobolyev <sobomax@sippysoft.com>
2026-03-19 10:04:44 -0700

    Expose rtpp_packetport and related methods.

--- src/librtpproxy.h
+++ src/librtpproxy.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Sippy Software, Inc., http://www.sippysoft.com
+ * Copyright (c) 2023-2026 Sippy Software, Inc., http://www.sippysoft.com
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,14 +27,42 @@
 
 #pragma once
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef void (*rtp_packet_ext_dtor_t)(void *);
+
 struct rtpp_cfg;
+struct SPMCQueue;
 
 void rtpp_shutdown(struct rtpp_cfg *);
 struct rtpp_cfg *rtpp_main(int argc, const char * const *argv);
+
+struct rtpp_packetport {
+    struct SPMCQueue *in;
+    struct SPMCQueue *out;
+};
+
+struct rtp_packet_ext {
+    const void *data;
+    int dlen;
+    unsigned int port;
+};
+
+struct rtpp_packetport *rtpp_packetport_ctor(unsigned int);
+void rtpp_packetport_push(struct rtpp_packetport *, struct rtp_packet_ext *);
+int rtpp_packetport_try_push(struct rtpp_packetport *, struct rtp_packet_ext *);
+struct rtp_packet_ext *rtpp_packetport_try_pop(struct rtpp_packetport *);
+size_t rtpp_packetport_try_pop_many(struct rtpp_packetport *,
+  struct rtp_packet_ext **, size_t);
+unsigned int rtpp_packetport_next_in_port(struct rtpp_packetport *);
+void rtpp_packetport_dtor(struct rtpp_packetport *);
+struct rtp_packet_ext *rtp_packet_ext_ctor(int, unsigned int, const void *,
+  rtp_packet_ext_dtor_t, void *);
+void rtp_packet_ext_dtor(struct rtp_packet_ext *);
 
 #ifdef __cplusplus
 }
